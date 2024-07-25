6add0fe0dfa590fd0f15cacd9d266a1c5b413dc2
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2024-07-26 03:05:18 +0530

    Time: 585 ms (23.42%) | Memory: 19.8 MB (39.14%) - LeetSync

---
+++ 127-word-ladder/word-ladder.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        int ans = 0;
+        queue<pair<string,int>>q;
+        q.push({beginWord,1});
+        set<string>se;
+        for(auto i : wordList) se.insert(i);
+        if(se.find(endWord) == se.end()){
+            return 0;
+        }
+        while(!q.empty()){
+            auto itr = q.front();
+            q.pop();
+            if(itr.first == endWord)
+                ans = itr.second;
+            cout<<itr.first<<endl;
+            int slen = itr.first.size();
+            string it = itr.first;
+            for(int i=0;i<slen;i++){
+                for(int j=0;j<26;j++){
+                    string pref ,suff , cal;
+                    pref = it.substr(0,i);
+                    suff = it.substr(i+1,slen-i-1);
+                    cal = pref + char('a' + j) + suff;
+                    if( cal != itr.first && se.find(cal) != se.end()){
+                        q.push({cal,itr.second+1});
+                        se.erase(cal);
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
