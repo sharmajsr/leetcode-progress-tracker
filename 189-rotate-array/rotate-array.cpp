65db676914d2736d291c40429427253d97f6aff0
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2024-08-03 01:25:10 +0530

    Time: 17 ms (82.09%) | Memory: 27.6 MB (41.56%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        k = k%nums.size();
+        ios_base::sync_with_stdio(false);
+        k%=nums.size();
         reverse(nums.begin(),nums.end()-k);
         reverse(nums.end()-k,nums.end());
         reverse(nums.begin(),nums.end());
