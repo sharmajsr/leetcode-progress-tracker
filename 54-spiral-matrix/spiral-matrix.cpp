49ee5f53257cfbc46713270ccddcb11910868655
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2024-08-03 02:44:52 +0530

    Time: 3 ms (37.92%) | Memory: 8.3 MB (44.79%) - LeetSync

---
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        vector<int>ans;
+        int top = 0 ;
+        int left = 0;
+        int bottom = matrix.size()-1;
+        int right = matrix[0].size()-1;
+        while(top<=bottom && left<=right){
+            int f = 0;
+            // cout<<"left : "<<left<<" "<<"Right : "<<right<<endl;
+            for(int i = left; i<=right ; i++){
+                f=1;
+                // cout<<top<<" "<<i<<endl;
+                ans.push_back(matrix[top][i]);
+                // cout<<i<<" " <<matrix[top][i]<<"\n";
+            }
+            ++top;
+            
+            for(int i=top;i<=bottom;i++){
+                f=1;
+                ans.push_back(matrix[i][right]);
+                // cout<<matrix[i][right]<<" ";
+            }
+            --right;
+            
+            if(top<=bottom){
+                for(int i=right; i>=left ;i--){
+                    f=1;
+                    ans.push_back(matrix[bottom][i]);
+                    // cout<<matrix[bottom][i]<<" \n";
+                }
+                --bottom;
+            }
+            if(left<=right){
+            for(int i= bottom; i>=top ; i--){
+                f=1;
+                ans.push_back(matrix[i][left]);
+                // cout<<matrix[i][left]<<" ";
+            }
+            ++left;
+            }
+            if(!f) break;
+        }
+        return ans;
+    }
+};
