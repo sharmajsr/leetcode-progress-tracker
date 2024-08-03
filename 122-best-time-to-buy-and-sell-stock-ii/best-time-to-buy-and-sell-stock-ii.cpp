d7789aed55afa92a2e49615f70f730a27afd578b
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2024-08-03 13:26:47 +0530

    Time: 0 ms (100.00%) | Memory: 15.9 MB (54.12%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -4,16 +4,19 @@ public:
         
 
         int ans= 0 ,n = prices.size(),i=0;
-        while(i<n-1){
-            if(prices[i+1]>prices[i]){
-                int buyPrice = prices[i];
-                while( i<n-1 && prices[i+1] > prices[i]){
-                    ++i;
-                }
-                // cout<<buyPrice<<" "<<prices[i+1]<<" " <<i+1<<endl;
-                ans += prices[i] - buyPrice;
-            }
-            ++i;
+        // while(i<n-1){
+        //     if(prices[i+1]>prices[i]){
+        //         int buyPrice = prices[i];
+        //         while( i<n-1 && prices[i+1] > prices[i]){
+        //             ++i;
+        //         }
+        //         ans += prices[i] - buyPrice;
+        //     }
+        //     ++i;
+        // }
+        for(i=0;i<n-1;i++){
+            int temp = prices[i+1] - prices[i];
+            if(temp > 0 ) ans+=temp;
         }
         return ans;
     }
