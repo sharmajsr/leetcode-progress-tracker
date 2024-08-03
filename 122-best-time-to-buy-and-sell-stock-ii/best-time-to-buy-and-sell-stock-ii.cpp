00faac1be4013a25669690c37e54c71fa614e3e1
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2024-08-03 19:16:15 +0530

    Time: 0 ms (100.00%) | Memory: 15.9 MB (54.12%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        
-
-        int ans= 0 ,n = prices.size(),i=0;
-        // while(i<n-1){
-        //     if(prices[i+1]>prices[i]){
-        //         int buyPrice = prices[i];
-        //         while( i<n-1 && prices[i+1] > prices[i]){
-        //             ++i;
-        //         }
-        //         ans += prices[i] - buyPrice;
-        //     }
-        //     ++i;
-        // }
-        for(i=0;i<n-1;i++){
-            int temp = prices[i+1] - prices[i];
-            if(temp > 0 ) ans+=temp;
+    
+        int ans= 0 ,n = prices.size();
+        for(int i=0;i<n-1;i++){
+            if(prices[i+1]>prices[i]){
+                int buyPrice = prices[i];
+                while( i<n-1 && prices[i+1] > prices[i]){
+                    ++i;
+                }
+                ans += prices[i] - buyPrice;
+            }
+            // ++i;
         }
+        // for(i=0;i<n-1;i++){
+        //     int temp = prices[i+1] - prices[i];
+        //     if(temp > 0 ) ans+=temp;
+        // }
         return ans;
     }
 };
