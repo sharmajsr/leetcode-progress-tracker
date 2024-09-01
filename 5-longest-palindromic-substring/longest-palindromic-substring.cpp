9fba44046bc8dc02667000a4d0c6434b523a7109
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2024-09-02 04:40:45 +0530

    Time: 154 ms (34.01%) | Memory: 12.8 MB (65.58%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -6,17 +6,24 @@ public:
         int start = 0 ;
         int maxlen=1;
         memset(dp,false,sizeof(dp));
-        for(int i=0;i<=n;i++){
-            dp[i][i] =  1;
-        }
-        for(int k= 1 ;k<n;k++){
-            for(int i=0;i<n-k;i++){
-                int j = i+k;
-                if(j-i == 1){
+        // for(int i=0;i<=n;i++){
+        //     dp[i][i] =  1;
+        // }
+        // for(int k= 1 ;k<n;k++){
+        for(int diff = 0 ; diff<n ; diff++){
+            for(int i=0,j=i+diff ; j<n ; i++,j++){
+                // int j = i+k;
+                if( i==j) dp[i][j] = 1;
+                else if (diff == 1){
                     dp[i][j]= s[i] == s[j] ? 2 : 0;
                 }
                 else{
-                    dp[i][j] = (s[i] == s[j] && dp[i+1][j-1] ) ? j-i+1 : 0;
+                    if(s[i] == s[j] && dp[i+1][j-1]){
+                        dp[i][j] = dp[i+1][j-1] +2;
+                    }
+                    // else{
+                    //     dp[i][j] = dp[i+1][j-1] ;
+                    // }
                 }
                 if(dp[i][j] > maxlen){
                     maxlen = dp[i][j];
