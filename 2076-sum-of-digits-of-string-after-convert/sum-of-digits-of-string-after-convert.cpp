9d23cf8e293bec8d9a54fedea4a91bd80d98a0ac
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2024-09-14 00:34:04 +0530

    Time: 6 ms (18.06%) | Memory: 10.1 MB (29.42%) - LeetSync

--- 2076-sum-of-digits-of-string-after-convert/sum-of-digits-of-string-after-convert.cpp
+++ 2076-sum-of-digits-of-string-after-convert/sum-of-digits-of-string-after-convert.cpp
@@ -1,26 +1,32 @@
 class Solution {
 public:
-     
+    string getString(int p){
+        string result;
+        while(p >0 ){
+            int rem = p%10;
+            p/=10;
+            result += rem+'0';
+        }
+        return result;
+    }
     int getLucky(string s2, int k) {
         int  p = 0, n = s2.size();
         string s="";
         for(int i=0;i<n;i++){
             
-            s += to_string(s2[i]-'a'+1);
-            // cout<<s2[i] << " " <<s2[i]-'a'<<" "<<s<<endl;
+            s += getString(s2[i]-'a'+1);
+            
         }
-        cout<<"-> "<<s<<endl;
-        // p=int(st);
-        // "12552031545"
+        
         for(int i=0;i<k;i++){
             int temp = 0 ;
             string s1;
             for(int j=0; j < s.size(); j++){
                 temp += s[j]-'0'; 
                 s1+= to_string(s[j]-'0');
-                // cout<<s<<" "<<i<<" "<<s[j]-'0'<<" "<<temp<<endl;
+                
             }
-            s = to_string(temp);
+            s = getString(temp);
             p=temp;
         }
         return p;
