7e04bdf652c44bc5840d1809a8a51fab38978624
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2024-09-18 02:38:32 +0530

    Time: 149 ms (33.63%) | Memory: 12.8 MB (65.98%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,37 +1,33 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
+        // https://www.youtube.com/watch?v=fxwvVnBMN6I
         int n = s.size();
         int dp[n+1][n+1];
         int start = 0 ;
         int maxlen=1;
-        memset(dp,false,sizeof(dp));
-        // for(int i=0;i<=n;i++){
-        //     dp[i][i] =  1;
-        // }
-        // for(int k= 1 ;k<n;k++){
-        for(int diff = 0 ; diff<n ; diff++){
-            for(int i=0,j=i+diff ; j<n ; i++,j++){
-                // int j = i+k;
-                if( i==j) dp[i][j] = 1;
-                else if (diff == 1){
-                    dp[i][j]= s[i] == s[j] ? 2 : 0;
-                }
+        memset(dp,0,sizeof(dp));
+        for(int diff = 0 ;diff <n ; diff++){
+            for(int i=0 , j = i +diff ; j < n ; i++,j++){
+                if(i == j) dp[i][j] =1;
+                else if(diff == 1 and s[i]==s[j])   dp[i][j] = 2;
                 else{
-                    if(s[i] == s[j] && dp[i+1][j-1]){
-                        dp[i][j] = dp[i+1][j-1] +2;
+                    if(s[i] == s[j] and dp[i+1][j-1]){
+                        dp[i][j] = dp[i+1][j-1]+2;
                     }
-                    // else{
-                    //     dp[i][j] = dp[i+1][j-1] ;
-                    // }
-                }
-                if(dp[i][j] > maxlen){
-                    maxlen = dp[i][j];
-                    start =i;
-                }
+                } 
 
+                if(dp[i][j] > 0 and j-i+1 > maxlen ){
+                    start =i;
+                    maxlen = max(maxlen, j-i+1);
+                }
             }
         }
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<n;j++){
+        //         cout<<dp[i][j]<<" ";
+        //     }cout<<endl;
+        // }
         return s.substr(start,maxlen);
     }
 };
