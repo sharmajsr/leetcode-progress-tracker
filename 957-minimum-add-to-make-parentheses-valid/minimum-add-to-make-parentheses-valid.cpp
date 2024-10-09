eeec40e6394b6648fb3e5bb68c7f316ae2173928
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2024-10-10 04:15:30 +0530

    Time: 3 ms (43.11%) | Memory: 8 MB (32.67%) - LeetSync

---
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        int n = s.size();
+        stack<char>stk;
+        for(auto i : s){
+            if(stk.empty()){
+                stk.push(i);
+            }
+            else if(!stk.empty() and stk.top() =='(' and i ==')'){
+                stk.pop();
+            }
+            else{
+                stk.push(i);
+            }
+        }
+        return stk.size();
+    }
+};
