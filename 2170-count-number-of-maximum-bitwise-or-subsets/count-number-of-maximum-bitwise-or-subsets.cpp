6585e8c19cfffaebd7be44941b187d3e7c8c7a3e
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2024-10-19 00:47:05 +0530

    Time: 151 ms (11.61%) | Memory: 322.2 MB (6.50%) - LeetSync

--- 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.cpp
@@ -1,22 +1,27 @@
 class Solution {
 public:
-    int findSubset(int curVal, int curIndex,int targetVal,vector<int>nums,int &cnt){
+    int findSubset(int curVal, int curIndex,int targetVal,vector<int>nums,int &cnt,vector<vector<int>>&dp){
         
         
         if(curIndex == nums.size()){
             return curVal == targetVal ? 1 : 0;
         }
 
-        return findSubset( curVal ,curIndex+1,targetVal,nums,cnt) + 
-                findSubset( curVal | nums[curIndex] ,curIndex+1,targetVal,nums,cnt);
+        if(dp[curIndex][curVal] != -1) {
+            return dp[curIndex][curVal];
+        }
+
+        return dp[curIndex][curVal] = findSubset( curVal ,curIndex+1,targetVal,nums,cnt,dp) +
+                findSubset( curVal | nums[curIndex] ,curIndex+1,targetVal,nums,cnt,dp);
 
     }
     int countMaxOrSubsets(vector<int>& nums) {
+        
         int n = nums.size(),cnt = 0;
         int maxBitwise = 0 ;
         for(auto i : nums) maxBitwise |= i;
-        int ans = findSubset(0,0,maxBitwise,nums,cnt);
-        // cout<<ans << " " <<cnt<<endl;
+        vector<vector<int>>dp(n+1,vector<int>(maxBitwise+1,-1));
+        int ans = findSubset(0,0,maxBitwise,nums,cnt,dp);
         return ans;
     }
 };
