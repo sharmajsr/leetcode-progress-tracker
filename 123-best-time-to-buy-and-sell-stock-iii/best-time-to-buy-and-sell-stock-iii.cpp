7cc2d154bea9e273382cb251564f3ea7a7909137
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2024-10-21 15:22:58 +0530

    Time: 4 ms (99.66%) | Memory: 84.1 MB (74.94%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,15 +1,22 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
-        if(prices.size() == 0 ) return 0;
-        int a = INT_MIN , b = 0;
-        int c = INT_MIN ,d = 0 ;
-        for(auto i:prices){
-            a = max(a, -i);
-            b = max(b, a+i);
-            c = max(c, b-i);
-            d = max(d, c+i);
+    int maxProfit(vector<int>& a) {
+        int ans = 0, n= a.size() , i = 0,firstTransaction;
+        vector<int>leftMaxArray(n,0);
+        int mini = a[0];
+        int maxi = a[n-1];
+        vector<int>rightMaxArray(n,0);
+        for(int i = 1 ; i < n ; i++){
+            leftMaxArray[i] = max( leftMaxArray[i-1], (a[i]-mini));
+            mini = min(mini,a[i]);
         }
-        return d;
+        for(int i = n-2 ; i >= 0  ; i--){
+            rightMaxArray[i] = max(rightMaxArray[i+1], (maxi-a[i]));
+            maxi = max(maxi,a[i]);
+        }
+        for(int i=0;i<n;i++){
+            ans = max(ans, leftMaxArray[i] + rightMaxArray[i]);
+        }
+        return ans;
     }
 };
