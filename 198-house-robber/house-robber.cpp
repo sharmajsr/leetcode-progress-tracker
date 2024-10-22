04b25447b4bc025f1faedfdfd17d42bbaea1a28d
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2024-10-23 02:13:08 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (86.10%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
-    int solve(vector<int>&nums,int n,vector<int>& dp){
-        if(n== 0)   return  dp[n] = nums[0];
-        if( n<0)    return 0;
-        if(dp[n]!=-1)   return dp[n];
-
-        int curr = nums[n]+ solve(nums,n-2,dp);
-        int prev = solve(nums,n-1,dp);
-
-        return dp[n] = max(curr,prev);
-    }
     int rob(vector<int>& nums) {
-        vector<int>dp(nums.size(),-1);
-        solve(nums,nums.size()-1,dp);
-        return dp[nums.size()-1];
-
+        if(nums.size() == 1)    return nums[0];
+        else if(nums.size() == 2)    return max(nums[0],nums[1]);
+        int rob1 =0 ;
+        int rob2 = 0;
+        for(auto i : nums){
+            int temp = max(rob2,i + rob1);
+            rob1 = rob2;
+            rob2 =temp;
+        }
+        return rob2;
     }
 };
