22187111c7f794aee9e1b42684c4e0d7966eb358
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2024-10-23 11:56:08 +0530

    Time: 52 ms (91.32%) | Memory: 21.1 MB (59.05%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,34 +1,27 @@
 class Solution {
 public:
-    int maxi(int a , int b){
-        if(a > b)   return a;
-        return b;
-    }
     bool canPartition(vector<int>& nums) {
-        int tsum = 0 ;
-        for(int i : nums)    tsum+=i;
-        if(tsum % 2 )   return false;
-        tsum /=2 ;
-        
-        int n = nums.size();
-        int t[n+1][tsum+1];
+        int tsum = 0,n =nums.size(); 
+        for(auto i: nums)   tsum+= i;
+        int halfSum = tsum/2;
+        if(tsum % 2 !=0 )   return false;
+        int dp[n+1][halfSum+1];
+        memset(dp,0,sizeof(dp));
         for(int i=0;i<=n;i++){
-            for(int j=0;j<=tsum;j++){
-                t[i][j] = 0;
-                if(j==0){
-                    t[i][j] = 1;
-                }
+            for(int j=0;j<=halfSum;j++){
+                if( i == 0 )        dp[i][j] = 0 ;
+                if( j== 0)          dp[i][j] = 1 ;
             }
         }
-        for(int i=1 ; i<=n ; i++){
-            for(int j=1 ; j<=tsum ; j++){
-                if( j >= nums[i-1]){
-                    t[i][j] = t[i-1][j]  || t[i-1][j-nums[i-1]] ;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=halfSum;j++){
+                if(j >= nums[i-1]){
+                    dp[i][j] = dp[i-1][j] or dp[i-1][j-nums[i-1]];
                 }else{
-                    t[i][j] = t[i-1][j];
+                    dp[i][j] = dp[i-1][j];
                 }
             }
         }
-        return t[n][tsum];
+        return dp[n][halfSum];
     }
 };
