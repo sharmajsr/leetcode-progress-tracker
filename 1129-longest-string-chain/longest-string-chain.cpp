6425b8bc5adf30eec0bcbfb6b48c1c595fcbdc44
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2024-11-02 11:51:23 +0530

    Time: 214 ms (32.65%) | Memory: 65.5 MB (34.90%) - LeetSync

--- 1129-longest-string-chain/longest-string-chain.cpp
+++ 1129-longest-string-chain/longest-string-chain.cpp
@@ -3,30 +3,6 @@ public:
     static bool cmp1(string &a,string &b){
         return a.size() < b.size();
     }
-    bool comp(string st1,string st2){
-        string s1,s2;
-        if(st1.size() > st2.size()){
-            s1 = st1;
-            s2 = st2;
-        }else{
-            s1 = st2;
-            s2 = st1;
-        }
-        int n = s1.size() , m = s2.size(),i=0,j=0;
-        if(abs(n-m) != 1)       return false;
-        while(i < n and j < m ){
-            if(s1[i] == s2[j]){
-                ++i;++j;
-            }
-            else{
-                ++i;
-            }
-        }
-        if( j == m and i== n-1) return true;
-        if( i == n and j==m) return true;
-        return false;
-
-    }
     int longestStrChain(vector<string>& words) {
         sort(words.begin(),words.end(),cmp1);
         int n = words.size() ,ans = 0 ;
@@ -39,13 +15,12 @@ public:
                 
                 nWord.erase(nWord.begin()+j);
                 lis[word] = max(lis[nWord]+1,lis[word]);
-                cout<<nWord<<" , "<<lis[word]<<endl;
+                // cout<<nWord<<" , "<<lis[word]<<endl;
 
             }
-            // cout<<endl;
             ans = max(ans,lis[word]);
         }
-        for(int i=0;i<n;i++)    cout<<words[i]<<" "<<lis[words[i]]<<endl;
+        // for(int i=0;i<n;i++)    cout<<words[i]<<" "<<lis[words[i]]<<endl;
         return ans;
     }
 };
