1093c32ae45dedbe52192f4d2afbaa0b6d352a07
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2024-11-02 07:47:17 +0530

    Time: 6 ms (5.45%) | Memory: 8.9 MB (22.17%) - LeetSync

---
+++ 2580-circular-sentence/circular-sentence.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isCircularSentence(string s) {
+        int n= s.size(),start = 0 ;
+        vector<string>str;
+        if(s[0] != s[s.size()-1])   return false;
+        for(int i= 0;i<n;i++){
+            if(s[i] == ' '){
+                str.push_back(s.substr(start,i-start));
+                start = i+1;
+            }
+        }
+        str.push_back(s.substr(start,n-start));
+        for(int i= 1 ;i <str.size() ; i++){
+            string curWord = str[i];
+            string prevWord = str[i-1];
+            cout<<curWord<<" "<<prevWord<<endl;
+            if(curWord[0] != prevWord[prevWord.size()-1]){
+                return false;
+            }
+        }
+        return true;
+        
+    }
+};
