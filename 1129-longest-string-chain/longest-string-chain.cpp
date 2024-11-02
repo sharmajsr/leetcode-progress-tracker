1b10b9915cfcc773c3dfd2cef6d4aa940951f0d6
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2024-11-02 11:51:00 +0530

    Time: 1377 ms (25.49%) | Memory: 65.7 MB (34.82%) - LeetSync

--- 1129-longest-string-chain/longest-string-chain.cpp
+++ 1129-longest-string-chain/longest-string-chain.cpp
@@ -30,21 +30,22 @@ public:
     int longestStrChain(vector<string>& words) {
         sort(words.begin(),words.end(),cmp1);
         int n = words.size() ,ans = 0 ;
-        // vector<int>lis(n,1);
         unordered_map<string,int>lis;
         for(string word: words){
-            // cout<<word<< " : ";
+            cout<<word<< " : ";
             for(int j=0;j<word.size();j++){
                 
-                string nWord = word.substr(0,j)+word.substr(j+1);
-                // cout<<nWord<<" , ";
-                // nWord.erase(nWord.begin()+j);
+                string nWord = word;//.substr(0,j)+word.substr(j+1);
+                
+                nWord.erase(nWord.begin()+j);
                 lis[word] = max(lis[nWord]+1,lis[word]);
+                cout<<nWord<<" , "<<lis[word]<<endl;
+
             }
             // cout<<endl;
             ans = max(ans,lis[word]);
         }
-        // for(int i=0;i<n;i++)    cout<<words[i]<<" "<<lis[words[i]]<<endl;
+        for(int i=0;i<n;i++)    cout<<words[i]<<" "<<lis[words[i]]<<endl;
         return ans;
     }
 };
