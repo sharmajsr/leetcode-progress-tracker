dfae5e780d496681f10ccc7611caa419ff2cee04
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2024-11-08 02:03:15 +0530

    Time: 7 ms (94.07%) | Memory: 11.7 MB (25.40%) - LeetSync

---
+++ 368-largest-divisible-subset/largest-divisible-subset.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<int> largestDivisibleSubset(vector<int>& nums) {
+        int maxLis = 0 , maxLisIndex = 0, n= nums.size();
+        vector<int>lis(n,1);
+        vector<int>dis(n);
+        vector<int>ans;
+        sort(nums.begin(),nums.end());
+        for(int i=1;i<n;i++){
+            dis[i]=i;
+            for(int j=0;j<i;j++){
+                if(nums[i]%nums[j] == 0 and lis[i] < lis[j]+1){
+                    lis[i] = lis[j]+1;
+                    dis[i] =j;
+                }
+            }
+            if(maxLis < lis[i] ){
+                    maxLis = lis[i];
+                    maxLisIndex = i;
+            }
+        }
+        for(int i=0;i<n;i++){
+            // cout<<i<<" "<<nums[i]<<" "<<lis[i]<<" "<<dis[i]<<endl;
+        }    
+        cout<<"Max lis index : "<<maxLisIndex<<endl;
+        while(dis[maxLisIndex] != maxLisIndex){
+            ans.push_back(nums[maxLisIndex]);
+            maxLisIndex =dis[maxLisIndex];
+        }
+        ans.push_back(nums[maxLisIndex]);
+        reverse(ans.begin(),ans.end());
+        // for(auto i : ans)   cout<<i<<"  ";
+        return ans;
+    }
+};
