2089e1709c53f632f8932f9d38d2529ca59fdb69
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2024-11-09 02:54:54 +0530

    Time: 0 ms (100.00%) | Memory: 13 MB (83.14%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -7,11 +7,12 @@ public:
         for(int i = 1 ; i < n ; i++){
             if(nums[i]> temp.back()){
                 temp.push_back(nums[i]);
+                ++ans;
             }else{
                 int ind = lower_bound(temp.begin(),temp.end(),nums[i])-temp.begin();
                 temp[ind] = nums[i];
             }
         }
-        return temp.size();
+        return ans;
     }
 };
