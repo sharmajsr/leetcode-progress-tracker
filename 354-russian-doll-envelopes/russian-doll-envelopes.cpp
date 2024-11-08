a875815b2f9646f6fec277f64cdc52876dc82637
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2024-11-09 03:04:24 +0530

    Time: 64 ms (24.35%) | Memory: 81.3 MB (78.21%) - LeetSync

--- 354-russian-doll-envelopes/russian-doll-envelopes.cpp
+++ 354-russian-doll-envelopes/russian-doll-envelopes.cpp
@@ -6,14 +6,7 @@ public:
     }
     int maxEnvelopes(vector<vector<int>>& envelopes) {
         int n = envelopes.size();
-        if(n==0)    return 0;
-        vector<pair<int,int>>env;
         vector<int>lis;
-        
-        int maxLis = 1 ;
-        for(auto envelope : envelopes){
-            env.push_back({envelope[0],envelope[1]});
-        }
         sort(envelopes.begin(),envelopes.end(),cmp);
         lis.push_back(envelopes[0][1]);
         for(int i=1;i<n;i++){
@@ -21,7 +14,6 @@ public:
             int ind = lower_bound(lis.begin(),lis.end(),ele) - lis.begin();
             if(ind >= lis.size())    lis.push_back(ele);
             else    lis[ind] = ele;
-            
         }
         return lis.size();
         
