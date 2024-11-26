ef58db3d69f3d69d2bfadd0da91deacea430a32a
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2024-11-27 02:03:30 +0530

    Time: 5 ms (70.05%) | Memory: 101.2 MB (53.85%) - LeetSync

--- 3189-find-champion-ii/find-champion-ii.cpp
+++ 3189-find-champion-ii/find-champion-ii.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     int findChampion(int n, vector<vector<int>>& edges) {
-        // int n = edges.size();
-        vector<int>vec(n,0);
-        int cnt = 0,ans = 0;
-        for(int i= 0;i<edges.size();i++){
-            ++vec[edges[i][1]];
+        vector<int>in(n,0);
+        queue<int>q;
+        vector<int>adj[101];
+        for(int i=0;i<edges.size();i++){
+            adj[edges[i][0]].push_back(edges[i][1]);
+            ++in[edges[i][1]];
         }
         for(int i=0;i<n;i++){
-            if(vec[i] == 0 ){
-                ++cnt;
-                ans= i ;
+            if(in[i] == 0 ){
+                q.push(i);
             }
         }
-        return cnt >1 ? -1 : ans;
+        if(q.size() == 1 )  return q.front();
+        // if(q.size() == 1 )
+        return -1;
     }
 };
