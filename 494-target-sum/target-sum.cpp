e9c51f3835a1308f8192d85a87fc79f7b5b93fd9
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2024-12-08 14:05:13 +0530

    Time: 1028 ms (5.02%) | Memory: 16 MB (38.41%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -30,44 +30,24 @@ public:
     //     return v[n] = solve(n-1,target,curr+nums[n-1],nums) + solve(n-1,target,curr-nums[n-1],nums);
         
     // }
-    // int findTargetSumWays(vector<int>& nums, int target) {
-    //     // v.resize(nums.size()+1,-1);
-    //     // return solve(nums.size(),target,0,nums);
-    //     int countOfZeros = 0,tsum = 0 ;
-    //     for(auto i : nums){ 
-    //         if( i == 0 ) ++countOfZeros ; 
-    //         tsum += i ;
-    //     }
-    //     if((tsum + target) %2 ==1  )    return 0;
-    //     int ans = countOfSubsetSum(nums.size(),target,nums);
-    //     cout<<ans<<endl;
-    //     return ans*pow(2,countOfZeros); 
-    // }
     int findTargetSumWays(vector<int>& nums, int target) {
-        int totalSum = 0;
-        for (int num : nums) {
-            totalSum += num;
-        }
-        
-        // If the target is out of range of possible sums
-        if (abs(target) > totalSum) return 0;
-        
-        // If totalSum + target is odd, no solution exists
-        if ((totalSum + target) % 2 != 0) return 0;
-        
-        // Calculate the subset sum we need to find
-        int subsetSum = (totalSum + target) / 2;
-        
-        // DP to count number of subsets with given sum
-        vector<int> dp(subsetSum + 1, 0);
-        dp[0] = 1;
-        
-        for (int num : nums) {
-            for (int j = subsetSum; j >= num; j--) {
-                dp[j] += dp[j - num];
+        // v.resize(nums.size()+1,-1);
+        // return solve(nums.size(),target,0,nums);
+        int countOfZeros = 0,tsum = 0 ;
+        vector<int>nums1;
+        for(auto i : nums){ 
+            if( i == 0 ) ++countOfZeros ; 
+            else{
+                nums1.push_back(i);
             }
+            tsum += i ;
         }
-        
-        return dp[subsetSum];
+        if(countOfZeros > 0 ) nums = nums1;
+        if(tsum + target < 0 )  return 0;
+        if((tsum + target) %2 ==1  )    return 0;
+        int newTarget = (tsum + target)/2;
+        int ans = countOfSubsetSum(nums.size(),newTarget,nums);
+        cout<<"countOfZeros : "<<countOfZeros<<endl;
+        return countOfZeros > 0 ? ans*pow(2,countOfZeros) :ans; 
     }
 };
