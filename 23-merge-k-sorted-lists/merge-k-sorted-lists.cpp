b5d9776be1461b8c7d8be6b9619b098cf676f93b
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2024-12-18 01:43:31 +0530

    Time: 39 ms (18.91%) | Memory: 19.2 MB (12.65%) - LeetSync

---
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        priority_queue< pair<int, ListNode*>,vector< pair<int, ListNode*> >,greater<pair<int, ListNode*>> > nodes;
+        
+        for(auto node: lists){
+            while(node != NULL){
+                nodes.push({node->val,node});
+                node=node->next;
+            }
+        }
+        ListNode* dummyNode = new ListNode();
+        ListNode* head =dummyNode;
+        // dummyNode->next = head
+        while(!nodes.empty()){
+            auto top = nodes.top();
+            cout<<top.first<<endl;
+            nodes.pop();
+            dummyNode->next =  top.second;
+            dummyNode = dummyNode->next;
+        }
+        return head->next;
+    }
+};
