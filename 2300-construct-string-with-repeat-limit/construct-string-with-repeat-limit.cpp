93cdc1887c46ddc3f156e93d956b207c392e0724
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2024-12-17 22:59:11 +0530

    Time: 121 ms (7.09%) | Memory: 99 MB (5.39%) - LeetSync

---
+++ 2300-construct-string-with-repeat-limit/construct-string-with-repeat-limit.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int findNextAvailableCharacter(int cur,vector<int>freq){
+        for(int i=cur-1;i>=0;i--){
+            if(freq[i] > 0 )    return i;
+        }
+        return -1;
+    }
+    string repeatLimitedString(string s, int k) {
+        string ans;
+        vector<int>freq(26,0);
+        for(auto i :s){
+            ++freq[i-'a'];
+        }
+        for(int i=25;i>=0;i--){
+            // cout<<char(i+'a')<<" "<<freq[i]<<endl;
+            
+                
+            while(freq[i] > 0 ){
+                for(int j=0;j<k and freq[i] > 0;j++){
+                    ans += i+'a';
+                    --freq[i];
+                }
+                if(freq[i]>0){
+                    int nxtAvailableCharacterIndex = findNextAvailableCharacter(i,freq);
+                    if(nxtAvailableCharacterIndex == -1){
+                        return ans;
+                    }else{
+                        ans += nxtAvailableCharacterIndex+'a';
+                        --freq[nxtAvailableCharacterIndex];
+                    }
+                }
+            }
+                
+            
+        }
+        return ans;
+    }
+};
