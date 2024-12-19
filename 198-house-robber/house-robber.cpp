b99cd637ded3f848eef5310019b29a4a104cfc31
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2024-12-20 00:52:12 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (37.61%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        if(nums.size() == 1)    return nums[0];
-        else if(nums.size() == 2)    return max(nums[0],nums[1]);
-        int rob1 =0 ;
-        int rob2 = 0;
-        for(auto i : nums){
-            int temp = max(rob2,i + rob1);
-            rob1 = rob2;
-            rob2 =temp;
+        int r1 = 0 ,r2= 0;
+        for(int i=0;i<nums.size();i++){
+            int temp = max(nums[i] + r2,r1);
+            r2=r1;
+            r1=temp;
         }
-        return rob2;
+        return max(r2,r1);
     }
 };
