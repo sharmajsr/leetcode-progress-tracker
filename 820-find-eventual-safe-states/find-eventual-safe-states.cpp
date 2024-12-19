0c4538189546c94ed286a2296a69ea69b3b8ecea
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2024-12-19 22:08:23 +0530

    Time: 43 ms (60.94%) | Memory: 63.4 MB (47.80%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -2,28 +2,32 @@ class Solution {
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int n = graph.size();
+        vector<int>adj[10002];
+        vector<int>in(n,0);
         queue<int>q;
         vector<int>ans;
-        vector<int>in(n,0);
-        vector<int>adj[n];
         for(int i=0;i<n;i++){
-            for(int j=0 ; j < graph[i].size() ; j++ ){
-                ++in[i];
-                adj[graph[i][j]].push_back(i);
+            if(graph[i].size() > 0){
+                for(int j : graph[i]){
+                    ++in[i];
+                    adj[j].push_back(i);
+                }
             }
         }
-
         for(int i=0;i<n;i++){
-            if(in[i] == 0 ) q.push(i);
+            if(in[i] == 0  ) {
+                q.push(i);
+                // cout<<"Pushed into q "<<i<<endl;
+            }   
         }
         while(!q.empty()){
-            auto it = q.front();
+            auto top = q.front();
+            ans.push_back(top);
             q.pop();
-            ans.push_back(it);
-            for(auto j:adj[it]){
-                --in[j];
-                if(in[j] == 0 ){
-                    q.push(j);
+            for(auto i : adj[top]){
+                --in[i];
+                if(in[i] == 0 ){
+                    q.push(i);
                 }
             }
         }
