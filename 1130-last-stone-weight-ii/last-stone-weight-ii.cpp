0c7c096dcf758d383e07a54a97e9889b0fe66e88
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2024-12-27 17:11:06 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (36.09%) - LeetSync

--- 1130-last-stone-weight-ii/last-stone-weight-ii.cpp
+++ 1130-last-stone-weight-ii/last-stone-weight-ii.cpp
@@ -1,27 +1,34 @@
 class Solution {
 public:
     int lastStoneWeightII(vector<int>& stones) {
-        int tsum = 0 , n =stones.size() ,ans =INT_MAX,total ;
-        for(auto i : stones)    tsum += i;
-        total = tsum;
-        tsum/=2;
-        // int dp[n+1][tsum+1];
-        vector<vector<int>>dp(n+1,vector<int>(tsum+1,0));
-        for(int i=0;i<=n;i++){
-            dp[i][0]=1;
-        }
+        if(stones.size() ==1 )  return stones[0];
+        int n = stones.size(),tsum = 0 ,ans = INT_MAX;
+        for(auto i : stones)    tsum +=i;
+        int hsum = tsum/2;
+        vector<vector<int>>dp(n+1,vector<int>(hsum+1,0));
+        // int dp[n+1][hsum+1];
+        for(int i=0;i<=n;i++)   dp[i][0]=1;
+
         for(int i=1;i<=n;i++){
-            for(int j=1;j<=tsum;j++){
-                if(j>=stones[i-1]){
-                    dp[i][j] = dp[i-1][j] || dp[i-1][j-stones[i-1]]; 
+            for(int j=1;j<=hsum;j++){
+                if(j>= stones[i-1]){
+                    dp[i][j] = dp[i-1][j] or dp[i-1][j-stones[i-1]];
                 }else{
-                    dp[i][j] =dp[i-1][j];
+                    dp[i][j]=dp[i-1][j];
                 }
             }
         }
-        for(int i=0;i<=tsum;i++){
-            if(dp[n][i] > 0 ){
-                ans = total -2*i;
+
+        cout<<tsum<<endl;
+        for(int i=1;i<=hsum;i++){
+            // cout<<dp[n][i]<<" ";
+            if(dp[n][i] == 1 ){
+                // cout<<i<<" ";
+                int s1 = i;
+                int s2 = tsum - s1;
+
+                int diff = s1 > s2 ? s1-s2 : s2-s1;
+                ans = min( ans , diff );
             }
         }
         return ans;
