736234629a6bc5584c4667dc68798c621ecddefc
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2025-01-14 21:23:45 +0530

    Time: 31 ms (86.44%) | Memory: 82.3 MB (82.07%) - LeetSync

--- 883-car-fleet/car-fleet.cpp
+++ 883-car-fleet/car-fleet.cpp
@@ -8,21 +8,14 @@ public:
             vp.push_back({position[i],speed[i]});
         }
         sort(vp.begin(),vp.end(),greater<pair<int,int>>());
-        stack<double>st;
-        // st.push((target-vp[0].first)/vp[0].second);
         for(int i=0;i<n;i++){
             int pos = vp[i].first;
             int speed = vp[i].second;
             double tt = double(target-pos)/speed;
-            // tt /= speed;
             int tt1 = tt;
-            // if(st.empty()){
-            //     st.push(tt);
-            // }
             if(currTimeTaken < tt){
                 currTimeTaken = tt;
                 ++ans;
-                // st.push(tt);
             }
 
         }
