1e9ce484cc1bd2c4b2d36dcf27b74f13a7bb3a46
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2025-01-14 22:36:30 +0530

    Time: 55 ms (14.31%) | Memory: 97.5 MB (5.82%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -1,85 +1,73 @@
 class Solution {
 public:
-    vector<int>findNSE(vector<int>nums){
-        int n=nums.size();
+    vector<int> findNSE(vector<int> heights){
+        
         stack<pair<int,int>>st;
-        // vector<pair<int,int>>nse(n,0);
-        vector<int>nse(n,0);
-        for(int i= 0;i<n;i++){
+        
+        int n= heights.size();
+        vector<int>ans(n,0);
+        for(int i=0; i <n;i++){
             if(st.empty()){
-                st.push({nums[i],i});
-            }else if(!st.empty() and nums[i] >= st.top().first  ){
-                st.push({nums[i],i});
-            }
-            else if(!st.empty() and nums[i] < st.top().first){
-                while(!st.empty() and nums[i] < st.top().first){
-                    auto top = st.top();
+                st.push({heights[i],i});
+                
+            }else if(!st.empty() and st.top().first > heights[i]){
+                while(!st.empty() and st.top().first > heights[i]){
+                    auto top=st.top();
                     st.pop();
-                    nse[top.second]=i;
+                    ans[top.second] = i ;
                 }
-                st.push({nums[i],i});
+                st.push({heights[i],i});
+            }else{
+                st.push({heights[i],i});
             }
         }
         while(!st.empty()){
-            auto top = st.top();
+            ans[st.top().second]=n;
             st.pop();
-            nse[top.second]= n;
         }
-        return nse;
-    }
+        return ans;
 
-    vector<int>findPSE(vector<int>& heights){
-        int n=heights.size();
+    }
+    vector<int> findPSE(vector<int>heights){
         stack<pair<int,int>>st;
-        // vector<pair<int,int>>nse(n,0);
-        vector<int>pse(n,0);
+        int n= heights.size();
+        
+        vector<int>ans(n,0);
         for(int i=0;i<n;i++){
             if(st.empty()){
-                pse[i]=-1;
-                st.push({heights[i],i});
-            }
-            else if(!st.empty() and st.top().first >= heights[i]){
-                while(!st.empty() and st.top().first >= heights[i]){
-                    st.pop();
-                }
-                if(st.empty()){
-                    pse[i]=-1;
-                }
-                else if(!st.empty()){
-                    pse[i] = st.top().second;
-                }
                 st.push({heights[i],i});
             }
             else if(!st.empty() and st.top().first < heights[i]){
-                pse[i]=st.top().second;
+                ans[i]=st.top().second+1;
                 st.push({heights[i],i});
+            }else if(!st.empty() and st.top().first >= heights[i]){
+                while(!st.empty() and st.top().first >= heights[i]){
+                    // ans[st.top().second]=
+                    st.pop();
+                }
+                if(st.empty()){
+                    st.push({heights[i],i});
+                }else if(!st.empty()){
+                    ans[i]=st.top().second+1;
+                    st.push({heights[i],i});
+                }
             }
         }
-        return pse;
+        // while(!st.empty()){
+        //     ans[st.top().second]=0;
+        //     st.pop();
+        // }
+        return ans;
     }
     int largestRectangleArea(vector<int>& heights) {
-        int n= heights.size(),ans =0;
-
-
-        vector<int>nse,pse;
-        nse= findNSE(heights);
-        pse = findPSE(heights);
-        for(int i=0;i<n;i++){
-            // cout<<i<<" "<<nse[i]<<" "<<pse[i]<<endl;
-            ans = max(ans, heights[i] *(nse[i]-pse[i]-1));
+        int ans = 0 ;
+        if( heights.size() == 1 ) return heights[0];
+        vector<int>ns = findNSE(heights);
+        vector<int>ps = findPSE(heights);
+        for(int i=0;i<heights.size();i++){
+            ans = max(ans,heights[i]*(ns[i]-ps[i]));
+            // cout<<i<<" "<<heights[i]<<" "<<ns[i]<<" "<<ps[i]<<endl; 
         }
-        // for(int i=0;i<n;i++){
-        //     int curHeight = heights[i];
-        //     int left = i,right=i;;
-        //     while(left >=0 and heights[left] >= curHeight){
-        //         --left;
-        //     }
-        //     while(right < n and heights[right] >= curHeight){
-        //         ++right;
-        //     }
-        //     // cout<<left<<" "<<i<<" "<<right<<" "<<curHeight<<" "<<(right-left-1) * curHeight<<endl;
-        //     ans = max(ans,(right-left-1) * curHeight);
-        // }
         return ans;
     }
 };
