e88c229912325cebd2a473fd59c070c95a28bee5
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2025-01-14 21:22:22 +0530

    Time: 42 ms (60.05%) | Memory: 82.4 MB (67.34%) - LeetSync

--- 883-car-fleet/car-fleet.cpp
+++ 883-car-fleet/car-fleet.cpp
@@ -1,32 +1,30 @@
 class Solution {
 public:
     int carFleet(int target, vector<int>& position, vector<int>& speed) {
-        
-        int n = position.size();
-        stack<double>st;
+        int n = position.size(),ans =0 ,currTimeTaken = 0 ;
         vector<pair<int,int>>vp;
-        for(int i = 0 ; i < n ; i++){
+        for(int i= 0; i <n; i++){
             vp.push_back({position[i],speed[i]});
         }
-        sort(vp.begin(),vp.end());
-        for(int i= n-1;i>=0;i--){
-            double time1  = double(target - vp[i].first)/vp[i].second;
-
-            // if(!st.empty())
-            // cout<<float(time1)<<" " <<st.top()<<endl;
-            if(st.empty())  st.push(time1);
-            else if( time1 > st.top()){
-                st.push(time1);
+        sort(vp.begin(),vp.end(),greater<pair<int,int>>());
+        stack<double>st;
+        // st.push((target-vp[0].first)/vp[0].second);
+        for(int i=0;i<n;i++){
+            int pos = vp[i].first;
+            int speed = vp[i].second;
+            double tt = double(target-pos)/speed;
+            // tt /= speed;
+            int tt1 = tt;
+            // cout<<pos<<" "<<speed<<" "<<tt<<" "<<tt1<<" "<<currTimeTaken<<endl; 
+            if(st.empty()){
+                st.push(tt);
+            }
+            else if(st.top() < tt){
+                st.push(tt);
+                // ++ans;
             }
 
         }
         return st.size();
     }
 };
- 
-// c1 : 10 , 12 
-// c2 : 8  , 12
-// c3 : 0 , 1,2,3,4,5,6,7,8,9,10,11,12
-// c4 : 5  , 6, 7, 8, 9, 10, 11, 12
-// c5 : 3 , 6 , 9, 12
-//A car can not pass another car ahead of it.  c5 will reach with c4
