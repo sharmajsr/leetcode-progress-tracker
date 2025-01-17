25819bc75cdd703bf5a35ea30c19f54f9e8db8b4
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2025-01-17 11:59:28 +0530

    Time: 263 ms (5.05%) | Memory: 103.3 MB (5.16%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,23 +1,29 @@
 class Solution {
 public:
-    void solve(int target,vector<int>candidates,int start,vector<int>op,vector<vector<int>>&ans){
-        if(target == 0 ){
-            ans.push_back(op);
+    void solve(vector<int>op,set<vector<int>>&ans,vector<int>cand,int target,int curSum,int start){
+        if(curSum == target){
+            sort(op.begin(),op.end());
+            ans.insert(op);
             return;
         }
-        for(int i= start;i<candidates.size();i++){
-            if(target >= candidates[i]){
-                op.push_back(candidates[i]);
-                solve(target - candidates[i] , candidates , i,op,ans);
+        for(int i = start ; i < cand.size() ; i++){
+            if(target >= (curSum + cand[i]) ){
+                // solve(op,ans,cand,target,curSum  ,start+1);
+                // cout<<start<<" "<<i<<" "<<curSum<<" " <<cand[i]<<endl;
+                op.push_back(cand[i]);
+                solve(op,ans,cand,target,curSum +cand[i] ,start);
                 op.pop_back();
             }
         }
-
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int>>ans;
-        
-        solve(target, candidates, 0,{},ans);
-        return ans;
+        vector<int>op;
+        set<vector<int>>ans;
+        vector<vector<int>>ans1;
+        solve(op,ans,candidates,target,0,0);
+        for(auto i :ans){
+            ans1.push_back(i);
+        }
+        return ans1;
     }
 };
