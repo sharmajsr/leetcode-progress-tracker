cc858c61ed8d6e61a73c3d673115f63a5903c53c
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2025-01-24 23:37:11 +0530

    Time: 107 ms (6.01%) | Memory: 63.5 MB (56.95%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -2,32 +2,40 @@ class Solution {
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int n = graph.size();
-        vector<int>adj[10002];
-        vector<int>in(n,0);
+        vector<int>in(n+1,0);
+        vector<int>adj[10001];
         queue<int>q;
         vector<int>ans;
-        for(int i=0;i<n;i++){
-            if(graph[i].size() > 0){
-                for(int j : graph[i]){
-                    ++in[i];
-                    adj[j].push_back(i);
-                }
+        for(int i=0;i<graph.size();i++){
+            // int v0 = pai[0];
+            // int v1 = pai[1];
+            // adj[v1].push_back(v0);
+            
+            for(auto k : graph[i]){
+                adj[k].push_back(i);
+                ++in[i];
+            }
+            if(graph[i].size() == 0){
+                q.push(i);
+                
+                cout<<"Terminal nodes : "<<i<<endl;
             }
         }
-        for(int i=0;i<n;i++){
-            if(in[i] == 0  ) {
-                q.push(i);
-                // cout<<"Pushed into q "<<i<<endl;
-            }   
-        }
+        
+        // for(int i=0 ; i <= n ; i++){
+        //     if(in[i] == 0 ){
+        //         q.push(i);
+        //     }
+        // }
         while(!q.empty()){
             auto top = q.front();
             ans.push_back(top);
             q.pop();
-            for(auto i : adj[top]){
-                --in[i];
-                if(in[i] == 0 ){
-                    q.push(i);
+            for(auto adja : adj[top]){
+                --in[adja];
+                // cout<<top<<" "<<in[adja]<<endl;
+                if(in[adja] == 0 ){
+                    q.push(adja);
                 }
             }
         }
