e0feab353e9ceddd757f8bb8833c5cadf32c13bd
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2025-02-07 14:26:13 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (5.41%) - LeetSync

--- 1009-pancake-sorting/pancake-sorting.cpp
+++ 1009-pancake-sorting/pancake-sorting.cpp
@@ -9,36 +9,29 @@ public:
         }
         return curMaxElIndx;
     }
-    vector<int>reve(vector<int>arr,int k){
-        cout<<"starting rev : "<<endl;
-        reverse(arr.begin(),arr.begin()+k);
-        return arr;
-    }
+    // vector<int>reve(vector<int>arr,int k){
+    //     // cout<<"starting rev : "<<endl;
+    //     reverse(arr.begin(),arr.begin()+k);
+    //     return arr;
+    // }
     vector<int> pancakeSort(vector<int>& arr) {
         vector<int>ans;
         vector<int>n2 = arr;
         int j = arr.size() ;
         sort(n2.begin(), n2.end());
         while(n2 != arr){
-            cout<<"Value of j : "<<j<<endl;
+            // cout<<"Value of j : "<<j<<endl;
             int maxEleIdx = findIndexOfMaxElement( arr,j );
-            cout<<j<<" "<<maxEleIdx<<endl;
+            // cout<<j<<" "<<maxEleIdx<<endl;
             if(maxEleIdx != j){
                 // arr = reve(arr,maxEleIdx);
                 reverse(arr.begin(),arr.begin()+maxEleIdx+1);
-                // for(auto i : arr)   cout<<i<<" ";
-                // cout<<endl;
-                // arr = reve(arr,j) ;
                 reverse(arr.begin(),arr.begin()+j);
-                // for(auto i : arr)   cout<<i<<" ";
-                // cout<<endl;
                 ans.push_back(maxEleIdx+1);
                 ans.push_back(j);
                 
             }
             --j;
-            // for(auto i : arr)   cout<<i<<" ";
-            // cout<<endl;
             
             
         }
