534e89eb254f720d50d5814e34f5e261fae5cf34
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2025-02-09 20:39:23 +0530

    Time: 16 ms (60.00%) | Memory: 43.4 MB (100.00%) - LeetSync

--- 3748-sort-matrix-by-diagonals/sort-matrix-by-diagonals.cpp
+++ 3748-sort-matrix-by-diagonals/sort-matrix-by-diagonals.cpp
@@ -4,23 +4,29 @@ public:
         int n = grid.size() ;
         for(int k=0; k < n; k++){
             vector<int>temp;
+            priority_queue<int>maxH;
             for(int i=0;i<n-k;i++){
-                temp.push_back(grid[i+k][i]);
+                // temp.push_back(grid[i+k][i]);
+                maxH.push(grid[i+k][i]);
             }
             sort(temp.begin(), temp.end(),greater<int>());
             for(int i=0;i<n-k;i++){
-                grid[i+k][i] = temp[i];
+                grid[i+k][i] = maxH.top();
+                maxH.pop();
                 // temp.pop_front();
             }
         }
         for(int k=1; k < n; k++){
-            vector<int>temp;
+            // vector<int>temp;
+            priority_queue<int ,vector<int> , greater<int>>minH;
             for(int i=0;i<n-k;i++){
-                temp.push_back(grid[i][i+k]);
+                // temp.push_back(grid[i][i+k]);
+                minH.push(grid[i][i+k]);
             }
-            sort(temp.begin(), temp.end());
+            // sort(temp.begin(), temp.end());
             for(int i=0;i<n-k;i++){
-                grid[i][i+k] = temp[i];
+                grid[i][i+k] = minH.top();
+                minH.pop();
                 // temp.pop_front();
             }
         }
