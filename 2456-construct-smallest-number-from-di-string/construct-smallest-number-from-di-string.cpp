f5e9af7d42d064c7b42a6e3ac023a1d886b01331
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2025-02-20 01:19:31 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (45.17%) - LeetSync

--- 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.cpp
+++ 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.cpp
@@ -1,17 +1,7 @@
 class Solution {
 public:
-    string solve(int n,int &cur,string op,string pattern){
-        if(cur == n+1){
-            return op;
-        }
-        
-        if(pattern[cur-1] == 'I') return to_string(cur+1) + solve(n,cur,op,pattern);
-        cur+=1;
-        return solve(n,cur,op,pattern) + to_string(cur+1);
-    }
     string smallestNumber(string pattern) {
-        // int cur =0;
-        // return solve(pattern.size(),cur,"",pattern);
+    
         int n = pattern.size();
         string ans;
         for(int i=0;i<=n;i++){
