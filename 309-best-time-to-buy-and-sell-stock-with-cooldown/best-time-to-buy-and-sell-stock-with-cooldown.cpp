61e9ebec19d91fc9f8fbfd9999a47b872ad226fd
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2025-02-21 01:32:28 +0530

    Time: 0 ms (100.00%) | Memory: 16 MB (59.67%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -11,18 +11,15 @@ public:
     }
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>>dp(prices.size()+1,vector<int>(2,0));
+        vector<vector<int>>dp(prices.size()+2,vector<int>(2,0));
         // solve(prices,0,1,1,dp);
         for(int i=n-1;i>=0;i--){
             for(int j=0;j<2;j++){
                 if( j == 1 ){
                     dp[i][j]= max( -prices[i] + dp[i+1][0], dp[i+1][1]);
                 }else{
-                    int s1 = prices[i] ;
-                    if(i+2 < n){
-                        s1 += dp[i+2][1];
-                    }
-                    dp[i][j] = max( s1 , dp[i+1][0]);
+                    // int s1 = 
+                    dp[i][j] = max( prices[i] + dp[i+2][1] , dp[i+1][0]);
                 }
             }
         }
