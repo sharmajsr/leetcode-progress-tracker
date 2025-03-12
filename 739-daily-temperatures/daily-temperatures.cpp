7e2d5910f79c74d0c377782185da976a6a107212
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2025-03-12 21:10:23 +0530

    Time: 44 ms (7.13%) | Memory: 111.9 MB (12.46%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.cpp
+++ 739-daily-temperatures/daily-temperatures.cpp
@@ -1,31 +1,29 @@
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temp) {
-        stack<pair<int,int>>st;
         int n = temp.size();
-        vector<int>ans(n,0);
-        for(int i= 0 ; i< n ;i++){
-            // auto top = st.top();
-            if(st.empty() ){
+        vector<int>v(n,0);
+        stack<pair<int,int>>st;
+        for(int i= 0 ; i < n; i++){
+            if(st.empty()){
                 st.push({temp[i],i});
-            }
-            else if(!st.empty() && st.top().first >= temp[i]){
+            }else if(!st.empty() and st.top().first >= temp[i]){
                 st.push({temp[i],i});
-            }
-            else if(!st.empty() && temp[i] > st.top().first){
-                
-                    while(!st.empty() && temp[i]>st.top().first){
-                        // cout<<i<<" "<<st.top().first<<" " <<temp[i]<<endl;
-                        ans[st.top().second] = i-st.top().second;
-                        st.pop();
-                    }
-                    st.push({temp[i],i});
+            }else if(!st.empty() and st.top().first < temp[i]){
+                while(!st.empty() and st.top().first < temp[i]){
+                    auto top = st.top();
+                    st.pop();
+                    v[top.second]=  i - top.second;
                 }
+                st.push({temp[i],i});
+            }
         }
         while(!st.empty()){
-            ans[st.top().second]=0;
+            auto top = st.top();
             st.pop();
+            v[top.second]=  0;
         }
-        return ans;
+
+        return v;
     }
 };
