aefd9b2ecc76fcb953839a8bbcab7307cb5c5ce6
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2025-03-12 21:27:26 +0530

    Time: 35 ms (76.23%) | Memory: 89.8 MB (26.63%) - LeetSync

--- 883-car-fleet/car-fleet.cpp
+++ 883-car-fleet/car-fleet.cpp
@@ -1,24 +1,29 @@
 class Solution {
 public:
     int carFleet(int target, vector<int>& position, vector<int>& speed) {
-        int n = position.size(),ans =0 ;
-        double currTimeTaken = 0.0 ;
-        vector<pair<int,int>>vp;
-        for(int i= 0; i <n; i++){
-            vp.push_back({position[i],speed[i]});
+        int  n = position.size() ,cnt = 0;
+        vector<pair<int,double>>vp ; 
+        for(int i =  0 ;i  < n ;i++){
+            vp.push_back({position[i],(1.0*(target-position[i])/speed[i])});
         }
-        sort(vp.begin(),vp.end(),greater<pair<int,int>>());
-        for(int i=0;i<n;i++){
-            int pos = vp[i].first;
-            int speed = vp[i].second;
-            double tt = double(target-pos)/speed;
-            int tt1 = tt;
-            if(currTimeTaken < tt){
-                currTimeTaken = tt;
-                ++ans;
+        sort(vp.begin(),vp.end());
+        stack<float>st;
+        for(int i= n-1 ; i>=0 ;i--){
+            // cout<<vp[i].first<<" " <<vp[i].second<<endl; 
+            if(st.empty()){
+                st.push(vp[i].second);
+                ++cnt;
+            }else if(!st.empty() and st.top() < vp[i].second){
+                st.push(vp[i].second);
+                ++cnt;
             }
-
         }
-        return ans;
+        return cnt;
     }
 };
+// t = 12
+// position = [10 , 8 , 0 , 5 , 3]
+// t - pos = [ 2 , 4 , 12 , 7 , 9 ]
+// speed = [2 , 4 , 1 , 1 , 3]
+// time = [ 1, 1 , 12 , 7 ,3]
+// ts = [ 1, 1, 7 ,3 , 12]
