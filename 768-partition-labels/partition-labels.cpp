ce62eb743a690ac573b01da8441acff0cab95343
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2025-03-30 19:54:03 +0530

    Time: 6 ms (7.82%) | Memory: 9.2 MB (10.93%) - LeetSync

--- 768-partition-labels/partition-labels.cpp
+++ 768-partition-labels/partition-labels.cpp
@@ -5,18 +5,19 @@ public:
         vector<int>ans;
         int start = 0 ;
         unordered_map<char,int>um;
+        unordered_map<char,int>temp;
         for(auto i : s) ++um[i];
         for(int i= 0 ; i < s.size() ;i++){
-            alt.push_back(s[i]);
+            ++temp[s[i]];
             --um[s[i]];
             bool isValid = true;
-            for(auto i : alt){
-                if(um[i] != 0 ) isValid = false;
+            for(auto i : temp){
+                if(um[i.first] != 0 ) isValid = false;
             }
             if(isValid){
                 ans.push_back(i-start+1);
                 start = i+1 ;
-                alt = alt1;
+                temp.clear();
             }
         }
         return ans;
