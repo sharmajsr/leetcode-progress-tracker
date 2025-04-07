d79ee38a144353dda207a5b95f53e320c0f10ff3
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2025-04-08 00:19:47 +0530

    Time: 159 ms (25.13%) | Memory: 104.5 MB (15.94%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,29 +1,47 @@
 class Solution {
 public:
-    bool canPartition(vector<int>& nums) {
-        int n = nums.size();
-        int tsum = 0 ;
-        for(auto i : nums)  tsum += i;
-        if( tsum & 1 )  return false;
-        // cout<<"passed even check\n";
-        tsum/=2;
-        vector<vector<int>>dp(n+1,vector<int>(tsum+1,0));
-        // int dp[n+1][tsum+1]={0};
-        for(int i = 0 ; i <= n; i++) dp[i][0] = 1;
-
-        for(int i=1 ; i<=n ; i++){
-            for(int j=1 ;j <=tsum ; j++){
-                if( j < nums[i-1]){
-                    dp[i][j] = dp[i-1][j];
-                }
-                else{
-                    dp[i][j] = dp[i-1][j] || dp[i-1][j-nums[i-1]];
-                }
-                // cout<<dp[i][j]<< " "; 
-            }
-            // cout<<endl;
+    bool isSubsetSum(vector<int>&nums,int idx,int hsum,vector<vector<int>>&dp){
+        if( hsum == 0 or idx == 0){
+            dp[idx][hsum] = 0 ;
+            
+            if(hsum == 0)   return dp[idx][hsum] = true;
+            if(idx == 0 and hsum == nums[idx])  return dp[idx][hsum] =  true;
+            return false;
         }
+        if( dp[idx][hsum] != -1 ) return dp[idx][hsum];
+        bool not_pick = isSubsetSum(nums,idx-1,hsum,dp);
+        bool pick = false;
+        if( hsum >= nums[idx] )  pick = isSubsetSum(nums,idx-1,hsum-nums[idx],dp);
+        dp[idx][hsum] = 1 ;
+        return dp[idx][hsum] = pick or not_pick;
+    }
+    bool canPartition(vector<int>& nums) {
+        int tsum = 0 , n= nums.size();
+        for(auto i : nums){
+            tsum += i;
+        }
+        if(tsum % 2 )    return false;
+        tsum /=2 ;
+        // int hsum = tsum;
+        vector<vector<int>>dp(n+1,vector<int>(tsum+1,-1));
+        vector<vector<bool>>dp1(n+1,vector<bool>(tsum+1,false));
+        // cout<<tsum << " "<<nums.size()-1<<endl;
+        isSubsetSum(nums,nums.size()-1,tsum,dp);
+        // for(int idx = 0 ; idx< nums.size(); idx++){
+        //     for(int hsum = tsum ; hsum >=0 ;hsum--){
+        //         if( hsum == 0 or idx == 0){
+        //             if(hsum == 0)   dp[idx][0] = true;
+        //             if(idx == 0 and hsum == nums[idx])  dp[idx][hsum] = true;
+        //             dp[idx][hsum] = false;
+        //             continue;
+        //         }
+        //         bool not_pick = dp[idx-1][hsum];
+        //         bool pick = false;
+        //         if( hsum >= nums[idx] )  pick = dp[idx-1][hsum-nums[idx]];
+        //         dp[idx][nums] = pick or not_pick;
+        //     }
+        // }
+        return dp[n-1][tsum];
 
-        return dp[n][tsum];
     }
 };
