8c9db5d5c53ffefd8f53644cbb30ef5631ca9a32
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2025-04-08 01:01:33 +0530

    Time: 11 ms (67.38%) | Memory: 12.8 MB (20.08%) - LeetSync

--- 368-largest-divisible-subset/largest-divisible-subset.cpp
+++ 368-largest-divisible-subset/largest-divisible-subset.cpp
@@ -1,29 +1,31 @@
 class Solution {
 public:
     vector<int> largestDivisibleSubset(vector<int>& nums) {
-        int maxLis = 0 , maxLisIndex = 0, n= nums.size();
-        vector<int>lis(n,1);
-        vector<int>dis(n);
-        vector<int>ans;
-        sort(nums.begin(),nums.end());
-        for(int i=1;i<n;i++){
-            dis[i]=i;
-            for(int j=0;j<i;j++){
-                if(nums[i]%nums[j] == 0 and lis[i] < lis[j]+1){
+       int n = nums.size(),  maxLis = 1 , maxLisIdx = 0;
+       vector<int> ans;
+       vector<int>lis(n,1),prev(n);
+       sort(nums.begin(),nums.end());
+       for(int i=0;i<n;i++){
+        prev[i] = i ;
+            for(int j= 0;j <i ;j++){
+                if(nums[i] % nums[j] == 0 and lis[i] <= lis[j]){
                     lis[i] = lis[j]+1;
-                    dis[i] =j;
+                    prev[i] = j;
+                }
+                if(lis[i]  > maxLis){
+                    maxLis = lis[i];
+                    maxLisIdx = i;
+                    
                 }
             }
-            if(maxLis < lis[i] ){
-                    maxLis = lis[i];
-                    maxLisIndex = i;
-            }
         }
-        while(dis[maxLisIndex] != maxLisIndex){
-            ans.push_back(nums[maxLisIndex]);
-            maxLisIndex =dis[maxLisIndex];
+        // cout<<maxLis<<" "<<nums[maxLisIdx]<<" "<<lis[maxLisIdx]<<endl;
+        // for(int i=0 ;i < n ;i++)    cout<<i<<" "<<nums[i]<<" "<<lis[i]<<" "<<prev[i]<<endl;
+        while(prev[maxLisIdx] != maxLisIdx ){
+                ans.push_back(nums[maxLisIdx]);
+                maxLisIdx = prev[maxLisIdx];
         }
-        ans.push_back(nums[maxLisIndex]);
+        ans.push_back(nums[maxLisIdx]);
         reverse(ans.begin(),ans.end());
         return ans;
     }
