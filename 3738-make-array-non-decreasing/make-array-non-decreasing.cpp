8ebc04eec1baffd33edeef88681acaabfc8eb29d
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2025-04-20 19:46:59 +0530

    Time: 12 ms (27.50%) | Memory: 208.9 MB (39.02%) - LeetSync

--- 3738-make-array-non-decreasing/make-array-non-decreasing.cpp
+++ 3738-make-array-non-decreasing/make-array-non-decreasing.cpp
@@ -6,13 +6,7 @@ public:
         st.push(nums[0]);
         for(int i=1;i<n;i++){
             if(nums[i]>=st.top()){
-                runningSmall = 0;
-                st.push(nums[i]);
-            }else{
-                if(runningSmall == 0 ){
-                    ++cnt;
-                    runningSmall = 1;
-                }
+               st.push(nums[i]);
             }
         }
         return st.size();
