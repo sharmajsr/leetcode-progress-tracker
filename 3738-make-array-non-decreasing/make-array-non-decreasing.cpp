b895c5fed5d023c5a37fb27ae0bc42162b9dfc7c
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2025-04-20 19:48:11 +0530

    Time: 0 ms (100.00%) | Memory: 206.4 MB (56.46%) - LeetSync

--- 3738-make-array-non-decreasing/make-array-non-decreasing.cpp
+++ 3738-make-array-non-decreasing/make-array-non-decreasing.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     int maximumPossibleSize(vector<int>& nums) {
-        stack<int>st;
-        int n = nums.size(),cnt = 0,runningSmall = 0 ;
-        st.push(nums[0]);
+        // stack<int>st;
+        int lastEl =  nums[0];
+        int n = nums.size(),cnt = 1;
         for(int i=1;i<n;i++){
-            if(nums[i]>=st.top()){
-               st.push(nums[i]);
+            if(nums[i]>=lastEl){
+                lastEl = nums[i];
+                ++cnt;
             }
         }
-        return st.size();
+        return cnt;
     }
 };
