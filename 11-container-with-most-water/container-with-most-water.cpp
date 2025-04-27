00d94e8f9416ef5d5606293cefdd0e8f9ac55996
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2025-04-27 09:11:13 +0530

    Time: 2040 ms (5.50%) | Memory: 68.1 MB (12.68%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.cpp
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -1,12 +1,34 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        int i = 0 ,j = height.size() -1 ,ans = 0;
-        while(i<j){
-            ans = max(ans , min(height[i],height[j])*(j-i));
-            if(height[i]>height[j]) --j;
-            else    ++i;
+        
+        int n = height.size() , left = 0 ;
+        int right = n-1 , ans= 0 ;
+        vector<int>lMax(n,0), rMax(n,0);
+        lMax[0]   = height[0];
+        rMax[n-1] = height[n-1];
+        for(int i = 1 ; i<n ; i++){
+            lMax[i] = max(lMax[i-1],height[i]);
         }
-        return ans; 
+        for(int i = n-2 ; i >= 0 ; i-- ){
+            rMax[i] = max(rMax[i+1],height[i]);
+            cout<<i<<" "<<rMax[i]<<endl;
+        }
+        while(left<=right){
+            int dis = right- left;
+            int minH = min(lMax[left],rMax[right]);
+            ans = max(ans, dis*minH);
+            if(lMax[left] > rMax[right])    --right;
+            else ++left;
+        }
+        return ans;
+        
     }
 };
+// 1,8,6,2,5,4,8,3,7
+// 1 8 8 8 8 8 8 8 8
+// 8 8 8 8 8 8 8 7 7 
+// [8,7,2,1]
+//  8 8 8 8
+//  8 7 2 1
+// 
