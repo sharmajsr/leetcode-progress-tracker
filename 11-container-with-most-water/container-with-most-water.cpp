3e3b7e217506341d91356c32b8f9dbd6e5bdd9ef
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2025-04-27 09:14:06 +0530

    Time: 7 ms (8.13%) | Memory: 62.8 MB (78.82%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.cpp
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -1,34 +1,13 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        
-        int n = height.size() , left = 0 ;
-        int right = n-1 , ans= 0 ;
-        vector<int>lMax(n,0), rMax(n,0);
-        lMax[0]   = height[0];
-        rMax[n-1] = height[n-1];
-        for(int i = 1 ; i<n ; i++){
-            lMax[i] = max(lMax[i-1],height[i]);
-        }
-        for(int i = n-2 ; i >= 0 ; i-- ){
-            rMax[i] = max(rMax[i+1],height[i]);
-            
-        }
-        while(left<=right){
-            int dis = right- left;
-            int minH = min(lMax[left],rMax[right]);
-            ans = max(ans, dis*minH);
-            if(lMax[left] > rMax[right])    --right;
-            else ++left;
+        int n = height.size();
+        int i = 0 , j = n-1 ,ans = 0 ;
+        while(i<=j){
+            ans = max(ans,(j-i)*min(height[j],height[i]));
+            if(height[i] > height[j])   --j;
+            else ++i;
         }
         return ans;
-        
     }
 };
-// 1,8,6,2,5,4,8,3,7
-// 1 8 8 8 8 8 8 8 8
-// 8 8 8 8 8 8 8 7 7 
-// [8,7,2,1]
-//  8 8 8 8
-//  8 7 2 1
-// 
