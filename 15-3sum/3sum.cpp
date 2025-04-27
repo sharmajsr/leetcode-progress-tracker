ce02a4a128a50c247fea2e547a5c462aece84dbb
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2025-04-27 09:21:23 +0530

    Time: 194 ms (19.91%) | Memory: 48.8 MB (16.77%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,41 +1,33 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        set<vector<int>>ans;
-        vector<vector<int>> fans;
         int n = nums.size();
-        for(int i =0 ; i < n-2 ;i++){
-            int j = i+1;
-            int k = n-1;
+        set<vector<int>>se;
+        vector<vector<int>>ans;
+        sort(nums.begin(),nums.end());
+
+        for(int i = 0 ;i < n;i++){
+            int j = i+1 ,k = n-1;;
             while(j<k){
                 int sum = nums[i] + nums[j] + nums[k];
-                // cout<<nums[i]<<" "<<nums[j]<<" "<<nums[k]<<" "<<sum<<endl;
-                if(sum == 0){
+                if(sum == 0 ){
                     vector<int>temp;
                     temp.push_back(nums[i]);
                     temp.push_back(nums[j]);
                     temp.push_back(nums[k]);
-                    ans.insert(temp);
-                    while(j<k and nums[j] == nums[j+1])    j++;
-                    while(j<k and nums[k] == nums[k-1])    k--;
+                    se.insert(temp);
                     ++j;
                     --k;
-                    // cout<<j<<" "<<k<<endl;
-                    
-                }else if(sum >0){
+                    while(j < n and nums[j] == nums[j-1]) ++j;
+                    while(k>=0 and nums[k] == nums[k+1])   --k;
+                }else if(sum > 0 ){
                     --k;
                 }else{
                     ++j;
                 }
             }
         }
-        for(auto i : ans){
-            fans.push_back(i);
-        }
-        return fans;
+        for(auto i : se)   ans.push_back(i); 
+        return ans;
     }
 };
-
-// [-1,0,1,2,-1,-4]
-// [-4 , -1 ,-1 ,0 , 1, 2]
