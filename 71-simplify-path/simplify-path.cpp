8fb40b1c037d50299fb06563d9da1d9e5782e824
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2025-04-30 05:23:42 +0530

    Time: 11 ms (6.03%) | Memory: 11 MB (56.24%) - LeetSync

---
+++ 71-simplify-path/simplify-path.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        string ans,cur;
+        stack<string>st,st1;
+        int n = path.size();
+        for(int i = 1 ; i <n ;i++){
+            if(path[i] == '/' and path[i-1] == '/'){
+                    continue;
+            }
+            else if(path[i] == '/'){
+                if(cur == "" or cur == "."){
+                    cur = "";
+                    continue;
+                }else if(cur == ".."){
+                    if(!st.empty()) st.pop();
+                }else{
+                    st.push(cur);
+                }
+                cur = "";
+            }
+            else{
+                cur += path[i];
+            }
+        }
+        cout<<"after for : "<<cur<<endl;
+        if(cur == ".."){
+            if(!st.empty()) st.pop();
+        }else if(cur==""){}
+        else if(cur=="."){}
+        else if(cur=="/"){}
+        else{
+            st.push(cur);
+        }
+        while(!st.empty()){
+            // cout<<st.top()<<endl;
+            
+            st1.push(st.top());
+            st.pop();
+        }
+        while(!st1.empty()){
+            // if(st.top() == "" or st.top() == " "){
+            //     st.pop();
+            //     continue;
+            // } 
+            cout<<st1.top()<<endl;
+            ans += "/" + st1.top() ;
+            st1.pop();
+        }
+        if(ans.size()> 0 and ans[ans.size() -1] == '/' ){
+            ans = ans.substr(0,ans.size()-1) ;
+        }
+        return ans == "" ? "/" : ans;
+        
+    }
+};
