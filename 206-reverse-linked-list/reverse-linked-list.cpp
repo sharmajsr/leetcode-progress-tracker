61de36af5b81b2b1882d5efef740cbd02bb8db60
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2025-05-07 11:32:42 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (17.91%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.cpp
+++ 206-reverse-linked-list/reverse-linked-list.cpp
@@ -11,14 +11,14 @@
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-       ListNode* prev = NULL;
-       ListNode* next ; 
-       while(head){
-        next = head->next;
-        head->next = prev;
-        prev = head;
-        head =next;
-       }
-       return prev;
+        ListNode* prev = NULL;
+        ListNode* next ;
+        while(head!=NULL){
+            next = head->next;
+            head->next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
     }
 };
