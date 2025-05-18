ef7e5680d9c541ac5cdcec846283fd5350d7af22
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2025-05-19 03:04:31 +0530

    Time: 4 ms (21.14%) | Memory: 17.3 MB (7.15%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,28 +1,32 @@
 class Solution {
 public:
-    int solve(vector<int>&prices,int idx,int buy,int cooldown,vector<vector<int>>&dp){
-        if( idx >= prices.size())       return 0;
-        if( dp[idx][buy] != -1 )        return dp[idx][buy];
-        if(buy){
-            return dp[idx][buy]= max(-prices[idx] + solve(prices,idx+1,0,cooldown,dp),solve(prices,idx+1,1,cooldown,dp));
-        }else{
-            return dp[idx][buy]= max(prices[idx] + solve(prices,idx+1+cooldown,1,cooldown,dp),solve(prices,idx+1,0,cooldown,dp));
-        }
-    }
-    int maxProfit(vector<int>& prices) {
-        int n = prices.size();
-        vector<vector<int>>dp(prices.size()+2,vector<int>(2,0));
-        // solve(prices,0,1,1,dp);
-        for(int i=n-1;i>=0;i--){
-            for(int j=0;j<2;j++){
-                if( j == 1 ){
-                    dp[i][j]= max( -prices[i] + dp[i+1][0], dp[i+1][1]);
-                }else{
-                    // int s1 = 
-                    dp[i][j] = max( prices[i] + dp[i+2][1] , dp[i+1][0]);
-                }
+    int solve(vector<int>&prices,int buy,int c,int idx,vector<vector<vector<int>>>&dp){
+        // cout<<idx<<" "<<buy<<" "<<c<<endl;
+        if(idx == prices.size()-1 ){
+            if(buy == 1){
+                return 0;
+            }else{
+                return prices[prices.size()-1];
             }
         }
-        return dp[0][1];
+        if(dp[idx][buy][c] != -1)   return dp[idx][buy][c];
+        if(buy){
+            if(c > 0 ){
+                int get = -prices[idx] + solve(prices,0,1,idx+1,dp);
+                int nget = solve(prices,1,1,idx+1,dp);
+                // cout<<idx<<" "<<buy<<" "<<c<<" "<<get<<" "<<nget<<endl;
+                return dp[idx][buy][c] = max(get,nget);
+            }else{
+                return dp[idx][buy][c] = solve(prices,1,1,idx+1,dp);
+            }
+        }else{
+            return dp[idx][buy][c] = max(solve(prices,1,0,idx+1,dp)+prices[idx] , solve(prices,buy,1,idx+1,dp));
+        }
+
+    }
+    int maxProfit(vector<int>& prices) {
+        // reverse(prices.begin(),prices.end());
+        vector<vector<vector<int>>>dp(prices.size(),vector<vector<int>>(2,vector<int>(2,-1)));
+        return solve(prices,1,1,0,dp);
     }
 };
