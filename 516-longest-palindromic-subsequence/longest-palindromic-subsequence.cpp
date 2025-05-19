f88c8b88b6e0fb3d22f178e02681119c1c26fec9
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2025-05-20 01:38:33 +0530

    Time: 98 ms (12.53%) | Memory: 75.9 MB (43.99%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
@@ -1,34 +1,33 @@
 class Solution {
 public:
-    vector<vector<int>>dp;
-    int findLCS(string s,string t ,int x,int y ){
+    int solve(string s,string t,int idx1,int idx2,vector<vector<int>>&dp){
+        if(idx1 == 0 or idx2 == 0)    return 0;
+        if(dp[idx1][idx2]!= -1) return dp[idx1][idx2];
 
-        if(x == -1 or y == -1 ){
-            return 0;
+        if(s[idx1-1] == t[idx2-1]){
+            return 1+ solve(s,t,idx1-1,idx2-1,dp);
+        }else{
+            return max(solve(s,t,idx1-1,idx2,dp),solve(s,t,idx1,idx2-1,dp));
         }
-        if(dp[x][y] != -1)  return dp[x][y];
-        if(s[x] == t[y]){
-            return dp[x][y] = 1+ findLCS(s,t,x-1,y-1);
-        }
-        else
-        return dp[x][y] = max(findLCS(s,t,x-1,y),findLCS(s,t,x,y-1));
+
     }
     int longestPalindromeSubseq(string s) {
-        int n = s.size() ;
-        dp.resize(s.size()+1,vector<int>(s.size()+1,0));
         string t = s;
+        int n = s.size();
         reverse(t.begin(),t.end());
-        for(int i=1 ;i <=n; i++){
-            for(int j=1;j<=n; j++){
-                if(s[i-1] == t[j-1]){
-                    dp[i][j] = 1+dp[i-1][j-1];
+        // vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+        // return solve(s,t,n,n,dp);
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        for(int idx1 = 1 ; idx1<= n ;idx1++ ){
+            for(int idx2 = 1 ; idx2 <=n;idx2++){
+                if(s[idx1-1] == t[idx2-1]){
+                    dp[idx1][idx2] = 1+ dp[idx1-1][idx2-1];
                 }else{
-                    dp[i][j] = max(dp[i-1][j] , dp[i][j-1]);
+                    dp[idx1][idx2] =  max(dp[idx1-1][idx2],dp[idx1][idx2-1]);
                 }
             }
         }
-        // cout<<s<<" "<<t<<endl;
-        // findLCS(s,t,s.size()-1,t.size()-1);
-        return dp[s.size()][s.size()];
+        return dp[n][n];
+
     }
 };
