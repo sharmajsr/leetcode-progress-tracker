29f3029e4d3a6bb3dd95484236a35c52e6a2125d
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2025-05-25 16:42:33 +0530

    Time: 3 ms (71.26%) | Memory: 15.6 MB (49.47%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
-    void solve(int pos,int neg,vector<string>&ans,string op,int n ){
-        if(op.size() == n*2){
-            ans.push_back(op);
+    void solve(int pos,int neg,string cur,vector<string>&ans,int &n){
+        if(cur.size() == 2 * n ){
+            ans.push_back(cur);
             return;
         }
-        if(pos>0)   solve(pos-1,neg+1,ans,op+"(",n);
-        if(neg>0)   solve(pos,neg-1,ans,op+")",n);
+        if(pos > 0){
+            solve(pos-1,neg+1,cur+'(',ans,n);
+        }
+        if(neg>0){
+            solve(pos,neg-1,cur+')',ans,n);
+        }
     }
     vector<string> generateParenthesis(int n) {
         vector<string>ans;
-        solve(n,0,ans,"",n);
+        solve(n,0,"",ans,n);
         return ans;
     }
 };
