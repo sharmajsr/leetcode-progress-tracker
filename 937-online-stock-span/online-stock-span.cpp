10f1621ca499ba1dd9dec66b299b230b21d2c8f1
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2025-05-25 18:00:34 +0530

    Time: 343 ms (10.06%) | Memory: 92.9 MB (13.71%) - LeetSync

--- 937-online-stock-span/online-stock-span.cpp
+++ 937-online-stock-span/online-stock-span.cpp
@@ -1,34 +1,50 @@
 class StockSpanner {
 public:
-    stack<pair<int,int>>st;
+// stack<int>st;
+vector<int>v;
     StockSpanner() {
-        st.empty();  
+        // st.empty();
+        v.clear();
     }
     
     int next(int price) {
-        
-        int ans = 1;
-        if(st.empty()){
-            st.push({price,1});
-            return ans;
-        }
-        else if(st.top().first > price){
-            st.push({price,1});
-            return ans;
-        }
-        else{
-            // int del = 0;
-            while(!st.empty() and price >= st.top().first ){
-                    ans += st.top().second;
-                    st.pop();
-                    
+        // if(st.empty()){
+        //     st.push(price);
+        //     return 1;
+        // }
+        // else if(!st.empty() and st.top() > price){
+        //     st.push(price);
+        //     return 1;
+        // }else{
+        //     cout<<"Else  : "<<endl;
+        //     int cnt = 1 ;
+        //     while(!st.empty() and price >= st.top()){
+        //         cout<<st.top()<< " " <<price<<endl; 
+        //         ++cnt;
+        //         st.pop();
+        //     }
+        //     st.push(price);
+        //     return cnt;
+        // }
+        if(v.size() == 0){
+            v.push_back(price);
+            return 1;
+        }else if(v.size() != 0 and v[v.size()-1]> price){
+            v.push_back(price);
+            return 1;
+        }else{
+            int cnt = 1;
+            for(int i= v.size()-1 ; i >=0 ;i--){
+                if(price >= v[i]){
+                    ++cnt;
+                }else{
+                    break;
+                }
             }
-            // ans = del+1;
-            st.push({price,ans});
-            return ans;
+            v.push_back(price);
+            return cnt;
+
         }
-        
-        return ans;
     }
 };
 
