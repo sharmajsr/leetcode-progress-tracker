7ab009710e0ba1da92a6ccaed01b9e2e4583cd49
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2025-05-26 01:50:08 +0530

    Time: 1 ms (31.29%) | Memory: 8.5 MB (25.22%) - LeetSync

--- 778-reorganize-string/reorganize-string.cpp
+++ 778-reorganize-string/reorganize-string.cpp
@@ -1,35 +1,37 @@
 class Solution {
 public:
     string reorganizeString(string s) {
-        string temp;
+        unordered_map<char,int>um;
+        string ans;
         int n = s.size();
         if(n&1) n+=1;
-        n/=2;
-        if(s.size() <= 1)   return s;
-        unordered_map<char,int>um;
+        for(auto i : s){
+            ++um[i];
+        }
         priority_queue<pair<int,char>>maxH;
-        for(auto i : s) ++um[i];
         for(auto i : um){
-            cout<<i.first<<" " <<i.second<<endl;
-            if(i.second>n)   return "";
+            // cout<<i.first<< " "<<i.second<<" "<<n<<endl;
+            if(i.second > n/2)  return "";
             maxH.push({i.second,i.first});
         }
-        cout<<"size of maxH : " <<maxH.size()<<endl; 
-        while(maxH.size()>1){
-            // if(temp.size() == 0 ){
-                auto top1 = maxH.top();maxH.pop();
-                auto top2 = maxH.top();maxH.pop();
-                cout<<"top1 : "<<top1.first<<" "<<top1.second<<endl;
-                cout<<"top2 : "<<top2.first<<" "<<top2.second<<endl;
-                temp += top1.second;
-                temp += top2.second;
-                cout<<temp<<endl;
-                if(top1.first-1 >0 ) maxH.push({top1.first-1,top1.second});
-                if(top2.first-1 >0 ) maxH.push({top2.first-1,top2.second});
-            // }
+        while(!maxH.empty()){
+            auto e1 = maxH.top();maxH.pop();
+            ans += e1.second;
+            if(maxH.empty()) break;
+            pair<char,int>e2;
+            // if(maxH.size() > 0 )
+            e2 = maxH.top();maxH.pop();
+
+            
+            // if(maxH.size() > 0 )
+            ans += e2.second;
+            
+            if(e1.first-1 > 0)  maxH.push({e1.first-1,e1.second});
+            // if(maxH.size() > 0 )
+            if(e2.first-1 > 0)  maxH.push({e2.first-1,e2.second});
+            // cout<<maxH.size()<<" "<<ans<<endl;
             
         }
-        if(!maxH.empty())   temp += maxH.top().second;
-        return temp;
+        return ans ;
     }
 };
