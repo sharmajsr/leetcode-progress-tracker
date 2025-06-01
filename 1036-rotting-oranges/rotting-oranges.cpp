25a7a4b8f1885339746261ea359b8e2a849f3951
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2025-06-01 18:35:14 +0530

    Time: 3 ms (30.51%) | Memory: 17.7 MB (10.09%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,54 +1,55 @@
 class Solution {
+private:
+    void bfs(vector<vector<int>>& grid,int &ans,vector<vector<int>>& vis,int &cnt,queue<pair<pair<int,int>,int>>q){
+        // queue<pair<pair<int,int>,int>>q;
+        // vis[r][c] = 2;
+        // q.push({{r,c},0});
+        int rows[]={0,0,1,-1};
+        int cols[]={1,-1,0,0};
+        while(!q.empty()){
+            auto it = q.front();
+            q.pop();
+            
+            for(int i=0;i<4;i++){
+                int nr = rows[i] + it.first.first;
+                int nc = cols[i] + it.first.second;
+                int nq = it.second;
+                if(nr>=0 and nc>=0 and nr<grid.size() and nc<grid[0].size() and grid[nr][nc] == 1 ){
+                    q.push({{nr,nc},nq+1});
+                    ans = max(ans,nq+1);
+                    grid[nr][nc]=0;
+                }
+                
+            }
+        }
+
+    }
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        
-        queue<pair<int,int>>q;
-        int rows[] = {0,0,1,-1};
-        int cols[] = {1,-1,0,0};
-        int n = grid.size(), m = grid[0].size(),ans = 0,zc = 0;
-        // if(n == 1 and m == 1 and grid[0][0] == 0  ) return 0;
-        // if(n == 1 and m == 1 and grid[0][0] == 2  ) return 0;
-        // if(n == 0 and m== 0 and grid[0][0] = 0 or grid[0][0] ==1 ) return -1;
-        for(int i=0; i<n ;i++){
-            for(int j= 0; j< m;j++ ){
-                if(grid[i][j] == 2){
-                    q.push({i,j});
-                }
-                if(grid[i][j] == 0 ){
-                    ++zc;
+        int n = grid.size();
+        int m = grid[0].size();
+        queue<pair<pair<int,int>,int>>q;
+        int ans = 0 ,cntFresh = 0 ,cnt = 0 ;
+        vector<vector<int>>vis(n,vector<int>(m,0));
+        for(int i= 0;i<n;i++){
+            for(int j= 0; j<m;j++){
+                if(grid[i][j] == 2 ){
+                    q.push({{i,j},0});
                 }
             }
         }
-        if(n*m ==zc) return 0;
-        while(!q.empty()){
-            // auto top = q.front();
-            queue<pair<int,int>>qu;
-            ++ans;
-            while(!q.empty()){
-                auto top = q.front();
-                q.pop();
-                int p1 = top.first;
-                int q1 = top.second;
-                for(int i=0;i<4;i++){
-                    int nrow = rows[i] + p1;
-                    int ncol = cols[i] + q1; 
-                    if( nrow>=0 and ncol>=0 and nrow<n and ncol<m and grid[nrow][ncol] == 1 ){
-                        grid[nrow][ncol] = 2;
-                        qu.push({nrow,ncol});
-                    }
-                }
-            }
-            q=qu;
-        }
-        cout<<"n and m"<<n<<" "<<m<<endl;
-         for(int i=0; i<n ;i++){
-            for(int j= 0; j< m;j++ ){
-                cout<<grid[i][j]<<" ";
+        bfs(grid,ans,vis,cnt,q);
+        for(int i= 0;i<n;i++){
+            for(int j= 0; j<m;j++){
                 if(grid[i][j] == 1){
                     return -1;
                 }
-            }cout<<endl;
+            }
         }
-        return ans-1;
+        return ans;
     }
 };
+
+// 2 2 2
+// 2 2 1
+// 0 2 2
