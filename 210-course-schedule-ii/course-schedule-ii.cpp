ac63bde1e02fd4b8294eea11a8ca192aab5dc2ea
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2025-06-10 02:42:56 +0530

    Time: 3 ms (74.06%) | Memory: 18.4 MB (52.37%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,31 +1,30 @@
 class Solution {
 public:
-    vector<int> findOrder(int numC, vector<vector<int>>& prerequisites) {
-        vector<int>adj[2002];
-        vector<int>v;
-        vector<int>in(numC,0);
-        vector<int>toposort;
-        queue<int>q;
-        for(auto i : prerequisites){
-            // if(i[1] == i[0])    return false;
-            adj[i[1]].push_back(i[0]);
-            ++in[i[0]];
+    vector<int> findOrder(int numCourses, vector<vector<int>>& pre) {
+        vector<int>in(numCourses,0);
+        vector<int>adj[2001];
+        vector<int>temp;
+        for(int i= 0; i < pre.size(); i++){
+            ++in[pre[i][0]];
+            adj[pre[i][1]].push_back(pre[i][0]); 
         }
-        for(int i=0;i<numC;i++){
-            if(in[i] == 0 ) q.push(i);
+        queue<int>q;
+        for(int i= 0 ;i < numCourses; i++){
+            if(in[i] == 0 ){
+                q.push(i);
+            }
         }
         while(!q.empty()){
-            auto cur = q.front();
+            auto top = q.front();
             q.pop();
-            toposort.push_back(cur);
-            
-            for(auto j : adj[cur]){
-                --in[j];
-                if(in[j] == 0 ){
-                    q.push(j);
+            temp.push_back(top);
+            for(auto it : adj[top]){
+                --in[it];
+                if(in[it] == 0){
+                    q.push(it);
                 }
             }
         }
-        return  toposort.size() == numC ? toposort : v;
+        return temp.size() == numCourses ? temp : vector<int>{};
     }
 };
