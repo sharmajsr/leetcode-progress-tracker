09ee346fab01c18c8eace315fe7580b2c9f212b9
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2025-06-10 00:11:47 +0530

    Time: 26 ms (10.46%) | Memory: 105.6 MB (14.80%) - LeetSync

--- 463-island-perimeter/island-perimeter.cpp
+++ 463-island-perimeter/island-perimeter.cpp
@@ -1,31 +1,31 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& grid,int r, int c,vector<vector<int>>&vis,int &ans){
-        vis[r][c]=1;
+    void solve(vector<vector<int>>& grid,int x,int y,vector<vector<int>>& vis,int &perimeter){
         int rows[]={0,0,1,-1};
         int cols[]={1,-1,0,0};
-        for(int i= 0 ;i <4 ;i++){
-            int nr = r + rows[i];
-            int nc = c + cols[i];
-            if(nr>=0 and nc>=0 and nr < grid.size() and nc< grid[0].size() and grid[nr][nc] == 1 ){
-                if(vis[nr][nc] == 0){
-                    dfs(grid,nr,nc,vis,ans);
-                }
-            } 
-            else    ans++;
-            
+        vis[x][y]=1;
+        for(int i= 0 ; i<4 ;i++){
+            int nr = rows[i] + x;
+            int nc = cols[i] + y;
+            if(nr>=0 and nc>=0 and nr < grid.size() and nc < grid[0].size() and grid[nr][nc] ==1){
+                if( vis[nr][nc] == 0 )
+                solve(grid,nr,nc,vis,perimeter);
+            }else{
+                ++perimeter;
+            }
         }
     }
     int islandPerimeter(vector<vector<int>>& grid) {
-        int n = grid.size(), m = grid[0].size();
+        int perimeter = 0;
+        int n = grid.size() , m = grid[0].size();
         vector<vector<int>>vis(n,vector<int>(m,0));
-        int ans = 0 ;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(vis[i][j] == 0 and grid[i][j] == 1)
-                dfs(grid,i,j,vis,ans);
+        for(int i = 0; i <n ;i++){
+            for(int j= 0 ; j<m; j++){
+                if(grid[i][j] == 1 and vis[i][j] == 0 ){
+                    solve(grid,i,j,vis,perimeter);
+                }
             }
         }
-        return ans;
+        return perimeter;
     }
 };
