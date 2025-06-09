c814a5793ddc8e16c676eec093af91d2c96891da
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2025-06-10 02:20:48 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (9.04%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,55 +1,52 @@
 class Solution {
-private:
-    void bfs(vector<vector<int>>& grid,int &ans,vector<vector<int>>& vis,int &cnt,queue<pair<pair<int,int>,int>>q){
-        // queue<pair<pair<int,int>,int>>q;
-        // vis[r][c] = 2;
-        // q.push({{r,c},0});
-        int rows[]={0,0,1,-1};
-        int cols[]={1,-1,0,0};
+public:
+    int solve(vector<vector<int>>& grid,queue<pair<pair<int,int>,int>>q ,vector<vector<int>>&vis,int &cnt){
+        int rows[] = {0,0,1,-1}  , cols[] = {1,-1,0,0} ;
+        // int cnt = 0 ; 
+        
         while(!q.empty()){
-            auto it = q.front();
-            q.pop();
+            auto top = q.front();q.pop();
+            pair<int,int> cor = top.first;
+            int time = top.second ; 
             
-            for(int i=0;i<4;i++){
-                int nr = rows[i] + it.first.first;
-                int nc = cols[i] + it.first.second;
-                int nq = it.second;
-                if(nr>=0 and nc>=0 and nr<grid.size() and nc<grid[0].size() and grid[nr][nc] == 1 ){
-                    q.push({{nr,nc},nq+1});
-                    ans = max(ans,nq+1);
+            for(int i= 0; i < 4; i++ ){
+                int nr = cor.first + rows[i];
+                int nc = cor.second + cols[i];
+
+                if(nr >=0 and nr < grid.size() and nc >=0 and nc < grid[0].size() and grid[nr][nc] == 1  ){
+                    // vis[nr][nc] =1; 
+                    cnt = max(cnt,time+1);
                     grid[nr][nc]=0;
+                    q.push({{nr,nc},time+1});
                 }
-                
             }
         }
-
+        return cnt;
     }
-public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
-        queue<pair<pair<int,int>,int>>q;
-        int ans = 0 ,cntFresh = 0 ,cnt = 0 ;
+        
+        int n = grid.size() , m = grid[0].size() ,cnt = 0;
         vector<vector<int>>vis(n,vector<int>(m,0));
-        for(int i= 0;i<n;i++){
-            for(int j= 0; j<m;j++){
-                if(grid[i][j] == 2 ){
+        queue<pair<pair<int,int>,int>>q;
+        for(int i =0 ;i < n; i++){
+            for(int j= 0 ;j < m;j++){
+                if(grid[i][j]==2){
+                    vis[i][j]  = 1;
                     q.push({{i,j},0});
                 }
             }
         }
-        bfs(grid,ans,vis,cnt,q);
-        for(int i= 0;i<n;i++){
-            for(int j= 0; j<m;j++){
+        cnt = solve(grid,q,vis,cnt);
+
+        for(int i =0 ;i < n; i++){
+            for(int j= 0 ;j < m;j++){
                 if(grid[i][j] == 1){
+                    cout<<"fresh : "<<i<<" "<<j<<endl;
                     return -1;
                 }
             }
         }
-        return ans;
+
+        return cnt;
     }
 };
-
-// 2 2 2
-// 2 2 1
-// 0 2 2
