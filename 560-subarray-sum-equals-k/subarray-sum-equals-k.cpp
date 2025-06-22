bc527ca39cb4f57524c85535cd8638de90f753d1
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2025-06-22 07:03:53 +0530

    Time: 39 ms (78.34%) | Memory: 45.6 MB (45.57%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -7,20 +7,8 @@ public:
         for(int i= 0; i<n;i++){
             sum += nums[i];
             int diff = sum-k;
-            if(diff == 0 ){
-                if(um.find(0) == um.end()){
-                    cnt += 1;
-                    // um[0] = 1; 
-                }else
-                {
-                    // cout<<i<<" "<<sum<<" "<<um[diff]<<endl;
-                    cnt += um[diff];
-                    // ++um[0];
-                }
-            } 
-            else if(um.find(diff) != um.end()){
+            if(um.find(diff) != um.end()){
                 cnt += um[diff];
-                // ++um[sum];
             }
             ++um[sum];
         }
