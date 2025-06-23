c541040e8db174df91e516d99915065cce304a45
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2025-06-23 08:59:18 +0530

    Time: 255 ms (5.01%) | Memory: 22.9 MB (56.69%) - LeetSync

--- 143-reorder-list/reorder-list.cpp
+++ 143-reorder-list/reorder-list.cpp
@@ -13,11 +13,11 @@ public:
     ListNode* reverseList(ListNode* head){
         ListNode* prev = NULL;
         ListNode* next;
-        while(head!=NULL){
+        while(head){
             next = head->next;
-            head->next =prev;
+            head->next= prev;
             prev = head;
-            head= next;
+            head = next;
         }
         return prev;
     }
@@ -25,40 +25,55 @@ public:
         while(head){
             cout<<head->val<<" ";
             head=head->next;
-
-        }cout<<endl;
+        }
+        cout<<endl;
     }
     void reorderList(ListNode* head) {
-        if(!head or !head->next)  return ;
-        ListNode* dummyNode = new ListNode();
-        dummyNode->next =head;
+        if(head == NULL or head->next == NULL)    return ;
+        cout<<"Head not null \n";
         ListNode* slow =head;
         ListNode* fast = head;
         ListNode* prev;
         while(slow and fast and fast->next){
+            cout<<slow->val<<" "<<fast->val<<endl;
             prev = slow;
             slow = slow->next;
             fast = fast->next->next;
         }
-
-        ListNode* midNode ;
+        // cout<<"slow -> val "<<slow->val<<endl;
+        ListNode* midNode;
         if(!fast){
-            midNode = reverseList(prev);
-            // printList(midNode);
-            prev->next =NULL;
+            cout<<"even list\n";
+            midNode = prev->next;
+            cout<<midNode->val<<endl;
+            prev->next = NULL;
         }else{
-            midNode = reverseList(prev->next->next);
-            // printList(midNode);
-            prev->next->next = NULL;
+            cout<<"odd list\n";
+            midNode =slow->next;
+            cout<<midNode->val<<endl;
+            slow->next = NULL;
         }
-        while(head && midNode){
-            ListNode* temp = midNode->next;
-            midNode->next = head->next;
-            head->next = midNode;
-            head=head->next->next;
-            midNode = temp;
+        cout<<"midNode before rev ";
+        printList(midNode);
+        ListNode* l2 = reverseList(midNode);
+        ListNode* l1 = head;
+        cout<<"L1 ";
+        printList(l1);
+        cout<<"L2 ";
+        printList(l2);
+        
+        // 1 -> 2 -> NULL
+        // 3 -> 4 -> NULL
+        //     nxt
+        while(l1 and l2){
+            ListNode* next = l2->next;
+            l2->next = l1->next;
+            l1->next = l2;
+            l1 = l1->next->next;
+            l2 = next;
         }
-        // return dummyNode->next;
+
+
 
     }
 };
