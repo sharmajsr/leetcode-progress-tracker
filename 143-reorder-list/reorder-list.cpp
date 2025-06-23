76e866a83a056bfa4d839b1f94f0357e8d3ba7d0
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2025-06-23 08:59:42 +0530

    Time: 0 ms (100.00%) | Memory: 22.7 MB (80.06%) - LeetSync

--- 143-reorder-list/reorder-list.cpp
+++ 143-reorder-list/reorder-list.cpp
@@ -23,19 +23,19 @@ public:
     }
     void printList(ListNode* head){
         while(head){
-            cout<<head->val<<" ";
+            // cout<<head->val<<" ";
             head=head->next;
         }
         cout<<endl;
     }
     void reorderList(ListNode* head) {
         if(head == NULL or head->next == NULL)    return ;
-        cout<<"Head not null \n";
+        // cout<<"Head not null \n";
         ListNode* slow =head;
         ListNode* fast = head;
         ListNode* prev;
         while(slow and fast and fast->next){
-            cout<<slow->val<<" "<<fast->val<<endl;
+            // cout<<slow->val<<" "<<fast->val<<endl;
             prev = slow;
             slow = slow->next;
             fast = fast->next->next;
@@ -43,24 +43,24 @@ public:
         // cout<<"slow -> val "<<slow->val<<endl;
         ListNode* midNode;
         if(!fast){
-            cout<<"even list\n";
+            // cout<<"even list\n";
             midNode = prev->next;
             cout<<midNode->val<<endl;
             prev->next = NULL;
         }else{
-            cout<<"odd list\n";
+            // cout<<"odd list\n";
             midNode =slow->next;
             cout<<midNode->val<<endl;
             slow->next = NULL;
         }
-        cout<<"midNode before rev ";
+        // cout<<"midNode before rev ";
         printList(midNode);
         ListNode* l2 = reverseList(midNode);
         ListNode* l1 = head;
-        cout<<"L1 ";
-        printList(l1);
-        cout<<"L2 ";
-        printList(l2);
+        // cout<<"L1 ";
+        // printList(l1);
+        // cout<<"L2 ";
+        // printList(l2);
         
         // 1 -> 2 -> NULL
         // 3 -> 4 -> NULL
