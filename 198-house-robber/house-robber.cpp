1eaac2f338914b5af780916c3d356075a6874008
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2025-07-02 21:41:46 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (46.11%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,16 +1,20 @@
 class Solution {
-public:
-    int solve(vector<int>&nums,int idx,vector<int>&dp){
-        if(idx<0)   return 0;
-        if(idx == 0){
-            return dp[idx] = nums[idx];
-        } 
-        if(dp[idx]!=-1) return dp[idx];
-        return dp[idx] = max(solve(nums,idx-1,dp),solve(nums,idx-2,dp)+nums[idx]);
+private:
+    int solve(int idx, vector<int>&nums,vector<int>&dp){
+        if(idx < 0  ){
+            return 0;
+        }
+        if(dp[idx] != -1) return dp[idx];
+        int take = nums[idx] + solve(idx-2,nums,dp);
+        int not_take = solve(idx-1,nums,dp);
 
+        return dp[idx] = max(take,not_take);
     }
+public:
     int rob(vector<int>& nums) {
-        vector<int>dp(nums.size(),-1);
-        return solve(nums,nums.size()-1,dp);
+        
+        int n = nums.size() ;
+        vector<int>dp(n,-1);
+        return solve(n-1,nums,dp);
     }
 };
