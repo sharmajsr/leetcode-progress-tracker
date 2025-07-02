5230fcc41e3566ac3892dd9f20581891f9839927
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2025-07-03 00:51:47 +0530

    Time: 139 ms (44.73%) | Memory: 110.6 MB (31.96%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,47 +1,27 @@
 class Solution {
 public:
-    bool isSubsetSum(vector<int>&nums,int idx,int hsum,vector<vector<int>>&dp){
-        if( hsum == 0 or idx == 0){
-            dp[idx][hsum] = 0 ;
-            
-            if(hsum == 0)   return dp[idx][hsum] = true;
-            if(idx == 0 and hsum == nums[idx])  return dp[idx][hsum] =  true;
-            return false;
+    bool solve(vector<int>&nums, int idx,int target,vector<vector<int>>&dp){
+        if(idx == 0 ){
+            return target == nums[0];
         }
-        if( dp[idx][hsum] != -1 ) return dp[idx][hsum];
-        bool not_pick = isSubsetSum(nums,idx-1,hsum,dp);
-        bool pick = false;
-        if( hsum >= nums[idx] )  pick = isSubsetSum(nums,idx-1,hsum-nums[idx],dp);
-        dp[idx][hsum] = 1 ;
-        return dp[idx][hsum] = pick or not_pick;
+        if(dp[idx][target] != -1)   return dp[idx][target];
+        bool take = false;
+        if(target >= nums[idx])
+        take = solve(nums,idx-1,target-nums[idx],dp);
+        bool not_take = solve(nums,idx-1,target,dp);
+        // cout<<idx<<" "<<take<<" "<<not_take<<endl;
+        return dp[idx][target] = take or not_take;
     }
     bool canPartition(vector<int>& nums) {
-        int tsum = 0 , n= nums.size();
-        for(auto i : nums){
-            tsum += i;
-        }
-        if(tsum % 2 )    return false;
-        tsum /=2 ;
-        // int hsum = tsum;
-        vector<vector<int>>dp(n+1,vector<int>(tsum+1,-1));
-        vector<vector<bool>>dp1(n+1,vector<bool>(tsum+1,false));
-        // cout<<tsum << " "<<nums.size()-1<<endl;
-        isSubsetSum(nums,nums.size()-1,tsum,dp);
-        // for(int idx = 0 ; idx< nums.size(); idx++){
-        //     for(int hsum = tsum ; hsum >=0 ;hsum--){
-        //         if( hsum == 0 or idx == 0){
-        //             if(hsum == 0)   dp[idx][0] = true;
-        //             if(idx == 0 and hsum == nums[idx])  dp[idx][hsum] = true;
-        //             dp[idx][hsum] = false;
-        //             continue;
-        //         }
-        //         bool not_pick = dp[idx-1][hsum];
-        //         bool pick = false;
-        //         if( hsum >= nums[idx] )  pick = dp[idx-1][hsum-nums[idx]];
-        //         dp[idx][nums] = pick or not_pick;
-        //     }
-        // }
-        return dp[n-1][tsum];
+        int tsum = 0;
+        for(auto i : nums)  tsum += i;
+        if(tsum & 1)    return false;
 
+        int n = nums.size();
+
+        vector<vector<int>>dp(n,vector<int>(tsum/2+1,-1));
+        // tsum/=2;
+        cout<<tsum/2<<endl;
+        return solve(nums,n-1,tsum/2,dp);
     }
 };
