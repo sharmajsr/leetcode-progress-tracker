9fb8f1063b0103048fa50d4d032a82c8d6efb824
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2025-07-03 18:05:26 +0530

    Time: 7 ms (67.85%) | Memory: 11.9 MB (58.21%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -4,21 +4,15 @@ public:
         unordered_map<char,int>um;
         int i = 0 , j = 0 , n= s.size() ,maxL = 0;
         while(j < n){
-            ++um[s[j]];
-            // cout<<um.size()<<" "<<j-i+1<<endl;
-            if(um.size() == j-i+1){
-                maxL = max(maxL,j-i+1 );
-                ++j;
-            }else if(um.size()< j-i+1){
-                while(um.size() < j-i+1){
-                    --um[s[i]];
-                    if(um[s[i]] == 0 ){
-                        um.erase(s[i]);
-                    }
-                    ++i;
+            // um[s[j]]=j;
+            if(um.find(s[j]) != um.end()){
+                if(um[s[j]] >= i){
+                    i = um[s[j]] + 1;
                 }
-                ++j;
             }
+            maxL = max(maxL, j-i+1);
+            um[s[j]] = j;
+            ++j;
         }
         return maxL;
     }
