3b4fc6b199e492fa334aca5e03829ef98ae8c239
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2025-07-04 15:54:14 +0530

    Time: 2 ms (13.01%) | Memory: 8.8 MB (33.62%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -3,19 +3,28 @@ public:
     bool isValid(string s) {
         stack<char>st;
         for(auto i : s){
-            if( i == '(' or i == '{' or  i=='['){
+            if(i == '(' or  i== '{' or i == '['){
                 st.push(i);
             }
-            else if( i == '}' and !st.empty() and st.top() == '{' ){
-                st.pop();
-            }
-            else if( i == ')' and !st.empty() and st.top() == '(' ){
-                st.pop();
-            }
-            else if( i == ']' and !st.empty() and st.top() == '[' ){
-                st.pop();
-            }
-            else{
+            else if(!st.empty() and i == ')'){
+                if(st.top() == '('){
+                    st.pop();
+                }else{
+                    return false;
+                }
+            }else if(!st.empty() and i == '}'){
+                if(st.top() == '{'){
+                    st.pop();
+                }else{
+                    return false;
+                }
+            }else if(!st.empty() and i == ']'){
+                if(st.top() == '['){
+                    st.pop();
+                }else{
+                    return false;
+                }
+            }else{
                 st.push(i);
             }
         }
