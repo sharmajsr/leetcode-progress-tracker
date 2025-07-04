a216f1808e364bef7353f2b63cb0ff8ae6bda691
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2025-07-04 17:36:31 +0530

    Time: 0 ms (100.00%) | Memory: 27.2 MB (39.83%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -2,36 +2,22 @@ class Solution {
 public:
     int trap(vector<int>& height) {
         int n = height.size() ,ans =0 ;
-        // vector<int>left(n,0);
-        // vector<int>right(n,0);
-        // int temp = height[0];
-        // for(int i = 0 ; i<n ; i++){
-        //     temp = max(temp,height[i]);
-        //     left[i]=temp;
-        // } 
-        // temp = height[n-1] ;
-        // for(int i=n-1 ; i>=0 ; i--){
-        //     temp = max(temp,height[i]);
-        //     right[i]=temp;
-        // }
-        // for(int i=1;i<n-1;i++){
-        //     ans += min(left[i],right[i])-height[i];
-        // }
-        // return ans;
-        int left = 0 , right = n-1;
-        int leftBar = height[left] , rightBar = height[right];
-        while(left < right){
-            if(leftBar < rightBar){
-                ans += leftBar - height[left];
-                ++left;
-                leftBar = max(leftBar,height[left]);
-            }else{
-                ans += rightBar - height[right];
-                --right;
-                rightBar = max(rightBar,height[right]);
+        vector<int>left(n,0),right(n,0);
+        left[0]=height[0];
+        for(int i= 1 ;i < n;i++){
+            left[i]=max(left[i-1],height[i]);
+        }
+        right[n-1] = height[n-1];
+        for(int i= n-2;i>=0;i--){
+            right[i]=max(right[i+1],height[i]);
+        }
+        for(int i=1;i<n-1;i++){
+            if( height[i] < min(left[i],right[i])){
+                ans += min(left[i],right[i]) -height[i];
             }
         }
         return ans;
+        
     }
 };
 
