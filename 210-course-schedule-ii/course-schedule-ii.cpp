8208a1a2b577b1705665e31a2aee2d06077051cc
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2025-07-26 13:41:42 +0530

    Time: 3 ms (73.15%) | Memory: 18.9 MB (19.27%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,30 +1,35 @@
 class Solution {
 public:
-    vector<int> findOrder(int n, vector<vector<int>>& prerequisites) {
-        vector<int>temp,t2;
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
         vector<int>adj[2002];
-        vector<int>in(n);
-        for(auto i : prerequisites){
-            ++in[i[0]];
-            adj[i[1]].push_back(i[0]);
+        vector<int>in(numCourses,0);
+        vector<int>ans;
+        for(auto i :prerequisites ){
+            int v = i[0];
+            int u = i[1];
+            adj[u].push_back(v);
+            ++in[v];
         }
         queue<int>q;
-        for(int i = 0 ;i < n; i++){
+        for(int i = 0 ;i <numCourses;i++){
             if(in[i] == 0 ){
                 q.push(i);
             }
         }
-
         while(!q.empty()){
-            int fr = q.front();q.pop();
-            temp.push_back(fr);
+            auto fr = q.front();q.pop();
+            ans.push_back(fr);
             for(auto it : adj[fr]){
                 --in[it];
-                if( in[it] == 0 ){
+                if(in[it] == 0 ){
                     q.push(it);
                 }
             }
         }
-        return (temp.size() == n) ? temp : t2 ;
+        vector<int>a1; 
+        if(ans.size() == numCourses){
+            a1 =  ans;
+        }
+        return  a1;
     }
 };
