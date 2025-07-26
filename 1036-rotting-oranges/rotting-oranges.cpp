06666f685fe0ed2e5f588efebb779dea9f96f9e2
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2025-07-26 12:57:30 +0530

    Time: 0 ms (100.00%) | Memory: 17.3 MB (19.92%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,57 +1,58 @@
 class Solution {
 public:
-    int solve(queue<pair<int,int>>q,vector<vector<int>>&grid){
-        int rows[]={0,0,1,-1};
-        int cols[]={1,-1,0,0};
-        int time = 0 , cnt= 0 ;
-        while(!q.empty()){
-            queue<pair<int,int>>qu;
-            ++time;
-            while(!q.empty()){
-                auto fr = q.front();
-                q.pop();
-                int r = fr.first;
-                int c = fr.second;
-                for(int i= 0;i<4;i++){
-                    int nr = r + rows[i];
-                    int nc = c + cols[i];
-                    if(nr >=0 and nc>=0 and nr < grid.size() and nc < grid[0].size()  and grid[nr][nc] == 1){
-                        grid[nr][nc] =2;
-                        qu.push({nr,nc});
-                    }
+    int solve(queue<pair<int, pair<int, int>>> q, vector<vector<int>>& grid) {
+        int rows[] = {0, 0, 1, -1};
+        int cols[] = {1, -1, 0, 0};
+        int time = 0, cnt = 0;
+        while (!q.empty()) {
+
+            auto fr = q.front();
+            q.pop();
+            int r = fr.first;
+            int c = fr.second.first;
+            int tt = fr.second.second;
+            for (int i = 0; i < 4; i++) {
+                int nr = r + rows[i];
+                int nc = c + cols[i];
+                if (nr >= 0 and nc >= 0 and nr < grid.size() and
+                    nc < grid[0].size() and grid[nr][nc] == 1) {
+                    grid[nr][nc] = 2;
+                    time = tt+1;
+                    q.push({nr, {nc, tt + 1}});
                 }
             }
-            q=qu;
         }
         return time;
     }
     int orangesRotting(vector<vector<int>>& grid) {
-        int n = grid.size(), cnt = 0 ;
+        int n = grid.size(), cnt = 0;
         int m = grid[0].size();
-        queue<pair<int,int>>q;
-        int time = 0; 
-        for(int i= 0 ; i <n ;i++){
-            for(int j=0;j<m ;j++){
-                if(grid[i][j] == 2){
-                    q.push({i,j});
-                }else if(grid[i][j] == 1){
+        queue<pair<int, pair<int, int>>> q;
+        int time = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 2) {
+                    q.push({i, {j, 0}});
+                } else if (grid[i][j] == 1) {
                     ++cnt;
                 }
             }
         }
-        if(q.size() == 0 ){
-            if(cnt == 0 )   return 0;
-            else return -1;
-        }  
-        time = solve(q,grid);
-        for(int i= 0 ; i <n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j] == 1){
+        if (q.size() == 0) {
+            if (cnt == 0)
+                return 0;
+            else
+                return -1;
+        }
+        time = solve(q, grid);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 1) {
                     return -1;
                 }
             }
         }
-        
-        return time-1;
+
+        return time;
     }
 };
