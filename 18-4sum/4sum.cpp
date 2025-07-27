1dec86c16c8068c747f5c98c1e7f6d682b21381d
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2025-07-27 07:39:32 +0530

    Time: 52 ms (15.31%) | Memory: 22 MB (13.77%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -1,55 +1,50 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        set<vector<int>>se;
-        vector<vector<int>>ans;
         int n = nums.size();
+        if(n<4) return {};
+        vector<vector<int>>a1;
+        set<vector<int>>ans;
         sort(nums.begin(),nums.end());
-        for(int i = 0 ; i < n ; i++ ){
-            for(int j = i+1 ; j < n ; j++ ){
-                int k= j+1 , l = n-1;
-                while(k<l){
-                    long long t1 = nums[i] + nums[j] , t2 = nums[k] + nums[l],t3;
-                    t3 = t1 + t2;
-                    if(t3 == target){
+        // cout<<"After sorting\n";
+        for(int i = 0 ; i < nums.size()-3 ; i++){
+            // cout<<"Inside ith loop "<<i<<endl;
+            for(int j = i+1 ; j < nums.size()-2 ; j++ ){
+                // cout<<"Inside jth loop "<<j<<" "<<nums.size()<<endl;
+                int k = j+1;
+                int l = n-1;
+                while( k < l ){
+                    long long s3 = nums[i] + nums[j] ;
+                    long long s2 =  nums[k] + nums[l] ;
+                    long long s1 = s3 + s2;
+                    if(s1 == target ){
                         vector<int>temp;
-                        temp.push_back(nums[i]);
-                        temp.push_back(nums[j]);
-                        temp.push_back(nums[k]);
-                        temp.push_back(nums[l]);
-                        sort(temp.begin(),temp.end());
-                        se.insert(temp);
-                        ++k;
-                        --l;
-                        while(k<n and nums[k] == nums[k-1]) ++k;
-                        while(l>=0 and nums[l] == nums[l+1]) --l;
-                        
-                    }else if(t3 > target){
-                        --l;
-                    }else{
-                        ++k;
+
+                        temp.push_back(nums[i]);  temp.push_back(nums[j]);
+                        temp.push_back(nums[k]);  temp.push_back(nums[l]);
+                        // sort(temp.begin(),temp.end());
+                        // k+=1;
+                        // l-=1;
+                        ans.insert(temp);
+                        while( k < n-1 and nums[k] == nums[k+1])  k+=1;
+                        while( l > 0 and nums[l] == nums[l-1])    l-=1;
+                        k+=1;
+                        l-=1;
                     }
-                } 
-                // for(int k = j+1 ; k < n ; k++ ){
-                //     for(int l = k+1 ; l < n ; l++ ){
-                //         long long t1 = nums[i] + nums[j] , t2 = nums[k] + nums[l],t3;
-                //         t3 = t1 + t2;
-                //         if(t3 == target){
-                //             vector<int>temp;
-                //             temp.push_back(nums[i]);
-                //             temp.push_back(nums[j]);
-                //             temp.push_back(nums[k]);
-                //             temp.push_back(nums[l]);
-                //             sort(temp.begin(),temp.end());
-                //             se.insert(temp);
-                //         }
-                //     }
-                // }
+                    else if(s1 > target ){
+                        l -= 1;
+                    }else{
+                        k += 1;
+                    }
+                }
+
             }
         }
-        for(auto i : se){
-            ans.push_back(i);
+
+        // cout<<"After ith loop\n";
+        for(auto i : ans){
+            a1.push_back(i);
         }
-        return ans;
+        return a1;
     }
 };
