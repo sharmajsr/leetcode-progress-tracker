32ef3aacf3da9d1c2485eeeb42f484e04f481d00
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2025-07-27 06:38:22 +0530

    Time: 192 ms (14.86%) | Memory: 49.1 MB (12.39%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,37 +1,39 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        set<vector<int>>ans;
-        sort(nums.begin(),nums.end());
         int n = nums.size();
-        for(int i= 0; i< n-2; i++){
-            int j = i+1;
-            int k = n-1;
+        set<vector<int>>ans;
+        vector<vector<int>>a1;
+        sort(nums.begin(),nums.end());
+        for(int i= 0 ;i < nums.size() ; i++){
+            int j = i + 1 ;
+            int k = n - 1 ;
             while(j<k){
-                int sum = nums[i] + nums[j] + nums[k];
-                if(sum == 0 ){
+                int su1 = nums[i] + nums[j] + nums[k];
+                if(su1 == 0 ){
                     vector<int>temp;
                     temp.push_back(nums[i]);
                     temp.push_back(nums[j]);
                     temp.push_back(nums[k]);
                     ans.insert(temp);
-                    ++j;--k;
-                    while( j < n-1 and nums[j] == nums[j-1]) ++j;
-                    while( k > 0 and nums[k] == nums[k+1]) --k;
-                    // ++j;
-                    // --k;
-                    
-                }else if(sum > 0 ){
-                    --k;
-                }else{
-                    ++j;
+                    // j +=1 ;
+                    // k -=1 ;
+                    while(j < n-1 and nums[j] == nums[j+1])        j += 1 ;
+                    while(k > 0 and nums[k] == nums[k-1] )         k -= 1 ;
+                    j += 1 ;
+                    k -= 1 ;
+                }
+                else if(su1  < 0){
+                    j += 1;
+                }
+                else{
+                    k -= 1;
                 }
             }
         }
-        vector<vector<int>>ans1;
         for(auto i : ans){
-            ans1.push_back(i);
+            a1.push_back(i);
         }
-        return ans1;
+        return a1;
     }
 };
