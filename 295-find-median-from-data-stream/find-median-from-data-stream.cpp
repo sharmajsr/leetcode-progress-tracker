e4a0a9788779b503dba95a4b72e89f83d2ca8c58
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2025-07-28 19:09:53 +0530

    Time: 58 ms (59.61%) | Memory: 125 MB (43.63%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.cpp
+++ 295-find-median-from-data-stream/find-median-from-data-stream.cpp
@@ -1,35 +1,42 @@
 class MedianFinder {
 public:
-priority_queue<int>maxH;
-priority_queue<int,vector<int>,greater<int>>minH;
+    priority_queue<int>maxHeap;
+    priority_queue<int,vector<int>,greater<int>>minHeap;
     MedianFinder() {
-        // maxH.clear();
-        // minH.empty();
+        // maxHeap.empty();
+        // minHeap.empty();
     }
+// 
+//           
+//  2          3
+//  1          4
+// max       min
     
     void addNum(int num) {
-        if(maxH.empty()){
-            maxH.push(num);
-        }else if(!maxH.empty() and maxH.top()> num){
-            maxH.push(num);
+        if(maxHeap.empty()){
+            maxHeap.push(num);
+        }else if (num >= maxHeap.top() ){
+            minHeap.push(num);
         }else{
-            minH.push(num);
+            maxHeap.push(num);
         }
-        if(maxH.size()> 1 + minH.size()){
-            minH.push(maxH.top());maxH.pop();
+        if(maxHeap.size()>minHeap.size()+1){
+            minHeap.push(maxHeap.top());
+            maxHeap.pop();
         }
-        if(minH.size()> 1+ maxH.size()){
-            maxH.push(minH.top());minH.pop();
+        if(minHeap.size() > maxHeap.size()+1){
+            maxHeap.push(minHeap.top());
+            minHeap.pop();
         }
     }
     
     double findMedian() {
-        if(maxH.size() == minH.size()){
-            return (maxH.top()+minH.top())*0.5;
-        }else if(maxH.size() > minH.size()){
-            return maxH.top()*1.0;
+        if(minHeap.size() == maxHeap.size()){
+            return 1.0*(minHeap.top() + maxHeap.top()) * 0.5;
+        }else if(maxHeap.size() > minHeap.size()){
+            return maxHeap.top();
         }else{
-            return minH.top()*1.0;
+            return minHeap.top();
         }
     }
 };
