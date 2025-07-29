cc91dead238977b46a6380221347c48fcf05ad0b
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2025-07-29 15:09:50 +0530

    Time: 7 ms (59.23%) | Memory: 13.4 MB (27.82%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
 vector<vector<int>>dp;
     int solve(string &s1, string &s2, int x1, int x2){
-        if( x1 < 0 or x2 < 0 ){
+        if( x1 <= 0 or x2 <= 0 ){
             return max(x1,x2);
         }
         if(dp[x1][x2] != -1)    return dp[x1][x2];
-        if(s1[x1] == s2[x2]){
+        if(s1[x1-1] == s2[x2-1]){
             return   solve(s1,s2,x1-1,x2-1);
         }
         return dp[x1][x2] = 1 + min(solve(s1,s2,x1,x2-1), min(solve(s1,s2,x1-1,x2),solve(s1,s2,x1-1,x2-1)));
@@ -14,7 +14,7 @@ vector<vector<int>>dp;
     }
     int minDistance(string s1, string s2) {
         int n1 = s1.size(), n2 = s2.size();
-        dp.resize(n1,vector<int>(n2,-1));
-        return 1+solve(s1,s2,n1-1,n2-1);
+        dp.resize(n1+1,vector<int>(n2+1,-1));
+        return solve(s1,s2,n1,n2);
     }
 };
