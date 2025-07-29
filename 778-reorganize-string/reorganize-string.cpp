74e659f3ad5d0875e2e4c3cddde9894f5345d4bd
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2025-07-29 07:38:13 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (49.65%) - LeetSync

--- 778-reorganize-string/reorganize-string.cpp
+++ 778-reorganize-string/reorganize-string.cpp
@@ -1,36 +1,33 @@
 class Solution {
 public:
     string reorganizeString(string s) {
+        string ans;
+        priority_queue<pair<int,char>>maxH;
         unordered_map<char,int>um;
-        string temp;
         for(auto i : s){
             ++um[i];
         }
-        priority_queue<pair<int,char>>pq;
         for(auto i : um){
-            pq.push({i.second,i.first});
+            maxH.push({i.second,i.first});
         }
-        while(!pq.empty()){
-            cout<<"size : "<<pq.size()<<endl;
-            if(pq.size() == 1 and pq.top().first > 1) return "";
-            auto firs = pq.top(); pq.pop();
-            temp += firs.second;
-            pair<int,char> secon ;
-            if(!pq.empty()){
-                secon = pq.top(); pq.pop();
-                temp += secon.second;
+        while(!maxH.empty()){
+
+            auto fir = maxH.top();maxH.pop();
+            ans += fir.second;
+            if(maxH.empty() and fir.first > 1)  return "";
+
+            pair<int,char> sec ;
+            if(!maxH.empty()){
+                sec = maxH.top();maxH.pop();
+                ans += sec.second;
             }
             
-            if(firs.first-1>0) pq.push({firs.first-1,firs.second});
+            if(fir.first -1 > 0)    maxH.push({fir.first-1,fir.second});
+            if(sec.first -1 > 0 )   maxH.push({sec.first-1,sec.second});
             
-            if(secon.first-1>0) pq.push({secon.first-1,secon.second});
-
         }
-        return temp;
+        return ans;
     }
 };
-
-// a 3
-// b 1
-// 2 a
-// 0 b
+// aab , a=1 
+// 
