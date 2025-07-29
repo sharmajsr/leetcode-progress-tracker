a18623d22467deefaa3d6701b55ac02306ee8f7d
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2025-07-29 08:02:10 +0530

    Time: 7 ms (13.24%) | Memory: 14.6 MB (43.72%) - LeetSync

--- 1184-car-pooling/car-pooling.cpp
+++ 1184-car-pooling/car-pooling.cpp
@@ -1,31 +1,17 @@
 class Solution {
 public:
     bool carPooling(vector<vector<int>>& trips, int capacity) {
-        vector<int>v(1003,0);
-        priority_queue<
-        vector<int>,vector<vector<int>>,greater<vector<int>>
-        >minH;
+        vector<int>v(1002,0);
+        int temp = 0;
         for(auto i : trips){
-            // vector<int>temp = {};
-            // temp.push_back();
-            minH.push({i[1],i[2],i[0]});
+            v[i[1]]+=i[0];
+            v[i[2]]-=i[0];
         }
-        while(!minH.empty()){
-            
-            int start = minH.top()[0];
-            int end = minH.top()[1];
-            int cap = minH.top()[2];
-            if(cap > capacity)  return false;
-            minH.pop();
-            // cout<<start<<" "<<end<<" "<<cap<<endl;
-
-            v[start]-=cap;
-            v[end]+=cap;
-        }
-        v[0]+=capacity;
-        for(int i=1;i<1002;i++){
-            v[i]+=v[i-1];
-            if(v[i] < 0 )   return false;
+        for(int i= 0; i< 1002; i++){
+            temp += v[i];
+            if(temp > capacity){
+                return false;
+            }
         }
         return true;
     }
