259e36753df7ec0cda588320f423c7fb7f884820
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2025-08-06 02:27:15 +0530

    Time: 375 ms (22.52%) | Memory: 21.4 MB (31.48%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -1,31 +1,32 @@
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        int ans = 0;
+        int ans= 0 ;
         queue<pair<string,int>>q;
         q.push({beginWord,1});
-        unordered_set<string>se(wordList.begin(),wordList.end() );
-        se.erase(beginWord);
-        // for(auto i : wordList) se.insert(i);
-        // if(se.find(endWord) == se.end()){
-        //     return 0;
-        // }
+        set<string>se;
+        for(auto i : wordList){
+            if(i != beginWord)
+            se.insert(i);
+        }
+        
         while(!q.empty()){
-            auto itr = q.front();
-            q.pop();
-            if(itr.first == endWord)
-                return itr.second;
-            int slen = itr.first.size();
-            string it = itr.first;
-            for(int i=0;i<slen;i++){
+            auto fr = q.front();q.pop();
+            if(fr.first == endWord  ){
+                ans = fr.second;
+                break;
+            }
+            for(int i=0;i<fr.first.size();i++){
+                bool found =false;
                 for(int j=0;j<26;j++){
-                    string pref ,suff , cal;
-                    pref = it.substr(0,i);
-                    suff = it.substr(i+1,slen-i-1);
-                    cal = pref + char('a' + j) + suff;
-                    if( cal != itr.first && se.find(cal) != se.end()){
-                        q.push({cal,itr.second+1});
-                        se.erase(cal);
+                    string temp = fr.first;
+                    temp[i]=j+'a';
+                    if(se.find(temp)!=se.end()){
+                        // cout<<fr.first<<" "<<temp<<" "<<fr.second+1<<endl;
+                        q.push({temp,fr.second+1});
+                        se.erase(temp);
+                        found = true;
+                        // break;
                     }
                 }
             }
