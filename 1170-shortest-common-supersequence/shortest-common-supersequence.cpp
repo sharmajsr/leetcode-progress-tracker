b2400163d6675a7f7dd655e8d4725a74e93c51c5
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2025-08-07 23:22:36 +0530

    Time: 26 ms (50.83%) | Memory: 26.9 MB (19.79%) - LeetSync

--- 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
@@ -1,52 +1,46 @@
 class Solution {
 public:
-    int solve(string &s1, string &s2,int i ,int j ,vector<vector<int>>&dp ){
-        if( i == 0 or j == 0)   return dp[i][j]= 0;
-        if(dp[i][j] != -1)  return dp[i][j];
-        if(s1[i-1] == s2[j-1])  return dp[i][j]= 1+solve(s1,s2,i-1,j-1,dp);
-        return dp[i][j]= max(solve(s1,s2,i-1,j,dp),solve(s1,s2,i,j-1,dp));
+string solve(int n, int m,string &s1, string &s2){
+	// Write your code here.	
+	vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+	for(int i=1; i<=n;i++){
+		for(int j=1; j<=m ;j++){
+			if(s1[i-1] == s2[j-1]){
+				dp[i][j] = 1+ dp[i-1][j-1];
+			}else{
+				dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+			}
+		}
+	}
+	string ans;
+	int i = n, j= m ;
+	while(i>0 and j>0){
+		// cout<<i<<" "<<j<<" "<<ans<<endl;
+		if(s1[i-1] == s2[j-1]){
+			ans += s1[i-1];
+			--i;
+			--j;
+		}else if(dp[i-1][j]> dp[i][j-1]){
+            ans += s1[i-1];
+			--i;
+		}else{
+            ans += s2[j-1];
+			--j;
+		}
+	}
+    while(i>0){
+        ans += s1[i-1];
+			--i;
     }
+    while(j>0){
+         ans += s2[j-1];
+			--j;
+    }
+	reverse(ans.begin(),ans.end());
+	return ans;
+}
     string shortestCommonSupersequence(string s1, string s2) {
-        int n = s1.size() , m = s2.size() ,i,j;
-        string temp;
-        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-        solve(s1,s2,n,m,dp);
-        // for(int i=0;i<=n;i++){
-        //     for(int j=0;j<=m;j++){
-        //         cout<<dp[i][j]<<" ";
-        //     }cout<<endl;
-        // }
-        i = n ;
-        j = m ;
-        while(i> 0 and j > 0 ){
-            if(s1[i-1]==s2[j-1]){
-                temp += s1[i-1]; 
-                i -=1;
-                j -=1;
-                cout<<"if\n";
-            }else if(dp[i-1][j] > dp[i][j-1]){
-                 temp += s1[i-1];
-                --i;
-                cout<<"else1\n";
-            }else{
-               
-
-                temp += s2[j-1];
-                --j;
-                cout<<"else2\n";
-            }
-        }
-        cout<<i<<" "<<j<<endl;
-        while(i>0){
-            temp += s1[i-1];
-                --i;
-        }
-        while(j>0){
-            temp += s2[j-1];
-            --j;
-        }
-        reverse(temp.begin(),temp.end());
-        return temp;
-
+        int n = s1.size(), m= s2.size();
+        return solve(n,m,s1,s2);
     }
 };
