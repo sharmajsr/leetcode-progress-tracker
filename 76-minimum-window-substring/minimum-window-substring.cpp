fe12813e4d99b05be43b525082d3b6197b4a32de
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2025-08-08 02:13:46 +0530

    Time: 12 ms (47.53%) | Memory: 11.5 MB (66.41%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,70 +1,31 @@
 class Solution {
 public:
-    bool CheckAllElementsArePresent(unordered_map<char,int>&text_map, unordered_map<char,int>&pattern_map){
-        for(auto i : pattern_map){
-            if( text_map.find(i.first) == text_map.end()){
-                return false;
-            }
-            if(i.second > text_map[i.first]){
-                return false;
-            }
-        }
-        return true;
-    }
     string minWindow(string s, string t) {
-        int start =0 , pattern_map_size = 0, minLen = INT_MAX ;
-        unordered_map<char,int>um_text;
-        
-        unordered_map<char,int>um_pattern;
-        for(auto i : t) ++um_pattern[i];
-        pattern_map_size = um_pattern.size();
-        int i = 0 , j= 0, n =s.size();
-        while( j < n ){
-            ++um_text[s[j]];
-            if(um_text.size() < pattern_map_size){
-                ++j;
-            }else{
-                bool checkAllElementsArePresent = CheckAllElementsArePresent(um_text, um_pattern);
-                if(!checkAllElementsArePresent){
-                    ++j;
-                }else{
-                    if(minLen > j-i+1 ){
-                        minLen = j-i+1 ;
-                        start = i;
-                    }
-                    --um_text[s[i]];
-                    if(um_text[s[i]] == 0){
-                        um_text.erase(s[i]);
-                    }
-                    ++i;
-                    while(CheckAllElementsArePresent(um_text, um_pattern)){
-                        if(minLen > j-i+1 ){
-                            minLen = j-i+1 ;
-                            start = i;
-                        }
-                        --um_text[s[i]];
-                        if(um_text[s[i]] == 0){
-                            um_text.erase(s[i]);
-                        }
-                        ++i;
-                    }
-                    ++j;
+        int ans =1e9;
+        int start = -1 ,i = 0,j=0 , m = t.size() , n =s.size() ,cnt = 0;
+        unordered_map<int,int>um;
+        for(int i= 0; i <t.size(); i++){
+            ++um[t[i]];
+        } 
+        i=0;
+        while(j<n ){
+            if( um[s[j]] > 0 ) cnt += 1;
+            --um[s[j]];
+            while( cnt == m ){
+                if(j-i+1 < ans){
+                    ans= j-i+1;
+                    start = i;
                 }
-                
+                um[ s[i] ]++;
+                if(um[s[i]] > 0){
+                    cnt -=1;
+                }
+                ++i;
             }
-
-            
+            ++j;
         }
-        if(minLen == INT_MAX)   minLen =0;
-        return s.substr(start,minLen);
+        return start == -1? "": s.substr(start,ans);
         
+
     }
 };
-// A : 1 , B = 1 , C = 1 -> 3
-// ij
-// ADOBECODEBANC 
-// size_text = 4 
-// i = 0 
-// j = 1
-// A : 1 , D : 1 ,O : 1 , B : 1
-// 
