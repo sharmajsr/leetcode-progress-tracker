fe5b19e84b636c324f97c7458914d46d52f24f79
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2025-08-07 17:47:47 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (25.06%) - LeetSync

--- 778-reorganize-string/reorganize-string.cpp
+++ 778-reorganize-string/reorganize-string.cpp
@@ -12,19 +12,18 @@ public:
         }
         while(!maxH.empty()){
 
-            auto fir = maxH.top();maxH.pop();
+            auto fir = maxH.top(); maxH.pop();
             ans += fir.second;
-            if(maxH.empty() and fir.first > 1)  return "";
+            if(maxH.empty() and fir.first > 1)   return "";
 
-            pair<int,char> sec ;
+            pair<int,char> sec;
             if(!maxH.empty()){
                 sec = maxH.top();maxH.pop();
                 ans += sec.second;
             }
-            
-            if(fir.first -1 > 0)    maxH.push({fir.first-1,fir.second});
-            if(sec.first -1 > 0 )   maxH.push({sec.first-1,sec.second});
-            
+
+            if(fir.first-1>0)   maxH.push({fir.first-1,fir.second});
+            if(sec.first-1>0)   maxH.push({sec.first-1,sec.second});
         }
         return ans;
     }
