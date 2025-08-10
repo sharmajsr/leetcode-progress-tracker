2c2fdac6595bfb5f8ff0662116430928b548b459
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2025-08-11 04:16:33 +0530

    Time: 375 ms (49.26%) | Memory: 11.7 MB (23.71%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,37 +1,42 @@
 class Solution {
 public:
-    vector<vector<int>>vis;
-    bool solve(int p ,int q ,vector<vector<char>>& board , string &word,int idx){
-        vis[p][q]=1;
-        int rows[] = { 0,0,1,-1};
-        if(idx == word.size()){
+    vector<vector<int>> vis;
+    bool solve(vector<vector<char>>& board, string word, int r, int c, int idx
+               ) {
+        if (idx == word.size()) {
             return true;
         }
-        int cols[] = { 1,-1,0,0};
-        for(int i= 0 ;i <4 ;i++){
-            int nr = rows[i] + p;
-            int nc = cols[i] + q;
-            if(nr >=0 and nr < board.size() and
-                nc >=0 and nc <board[0].size() and board[nr][nc] == word[idx] and vis[nr][nc] == 0 and solve(nr,nc,board,word,idx+1)){
-                    return true;
-                }
+        vis[r][c] = 1;
+        int rows[] = {0, 0, 1, -1};
+        int cols[] = {1, -1, 0, 0};
+        for (int i = 0; i < 4; i++) {
+            int nr = rows[i] + r;
+            int nc = cols[i] + c;
+            if (nr >= 0 and nc >= 0 and nr < board.size() and
+                nc < board[0].size() and board[nr][nc] == word[idx] and
+                vis[nr][nc] !=1 and solve(board, word, nr, nc, idx + 1)) {
+
+                return true;
+            }
         }
-        vis[p][q]=0;
+        vis[r][c] = 0;
         return false;
     }
     bool exist(vector<vector<char>>& board, string word) {
-        int n = board.size() , m =board[0].size(); 
-        // vector<vector<int>>vis(n,vector<int>(m,0));
-        vis.resize(n,vector<int>(m,0));
-        for(int i = 0; i <n;i++){
-            for(int j= 0; j<m;j++){
-                if(board[i][j] == word[0] and solve(i,j,board,word,1)){
-                    vis[i][j]=1;
-                    return true;
-                    vis[i][j]=0;
+        int n = board.size(), m = board[0].size();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+
+                if (board[i][j] == word[0]) {
+                    cout<<i<<" "<<j<<endl;
+                    vis.resize(n, vector<int>(m, 0));
+                    if (solve(board, word, i, j, 1)) {
+                        return true;
+                    }
                 }
             }
         }
         return false;
-    }
-};
+        }
+    };
