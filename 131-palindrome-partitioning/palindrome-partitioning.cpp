d2b271135966f18afd0f58acc1858853fcba7241
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2025-08-11 04:00:55 +0530

    Time: 159 ms (7.14%) | Memory: 153.2 MB (6.64%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -9,7 +9,7 @@ public:
         return true;
     }
     void solve(string s,vector<string>path,vector<vector<string>>&res,int start){
-        if(start >= s.size()){
+        if(start == s.size()){
             res.push_back(path);
             return ;
         }
@@ -17,7 +17,6 @@ public:
         for(int i = start ; i < s.size(); i++){
             string temp = s.substr(start,i-start+1);
             bool pal_res = checkPal(s,start,i);
-            // cout<<s<<" "<<start<<" "<<temp<<" "<<pal_res<<endl;
             if(checkPal(s,start,i)){
                 path.push_back(temp);
                 solve(s,path,res,i+1);
