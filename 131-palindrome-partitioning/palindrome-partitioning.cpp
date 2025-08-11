3bf8494c0d09bcf48fbe3f8300735b318275a6be
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2025-08-11 16:22:06 +0530

    Time: 147 ms (9.77%) | Memory: 149.2 MB (7.25%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -1,33 +1,31 @@
 class Solution {
 public:
-    bool checkPal(string s, int i ,int j){
-        while(i<=j){
-            if( s[i++] != s[j--]){
+    bool chkPal(string s ,int i, int j){
+        while(i<j){
+            if(s[i++] != s[j--]){
                 return false;
             }
         }
         return true;
     }
-    void solve(string s,vector<string>path,vector<vector<string>>&res,int start){
-        if(start == s.size()){
-            res.push_back(path);
-            return ;
+    void solve(vector<vector<string>>&ans, vector<string>path,int idx, string s){
+        if(idx == s.size()){
+            ans.push_back(path);
+            return;
         }
-
-        for(int i = start ; i < s.size(); i++){
-            string temp = s.substr(start,i-start+1);
-            bool pal_res = checkPal(s,start,i);
-            if(checkPal(s,start,i)){
+        for(int i=idx ; i<s.size();i++){
+            string temp = s.substr(idx, i-idx+1);
+            bool chkpal = chkPal(s,idx,i);
+            if(chkpal){
                 path.push_back(temp);
-                solve(s,path,res,i+1);
+                solve(ans,path,i+1,s);
                 path.pop_back();
             }
         }
     }
     vector<vector<string>> partition(string s) {
         vector<vector<string>>ans;
-        vector<string>res;
-        solve(s,res,ans,0);
+        solve(ans,{},0,s);
         return ans;
     }
 };
