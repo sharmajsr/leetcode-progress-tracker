d5bb476d39e3b73f3be6ee726bb9416a6dc187ec
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2025-08-11 18:40:15 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (49.05%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.cpp
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -1,46 +1,36 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        vector<int>ans;
-        int top = 0 ;
-        int left = 0;
-        int bottom = matrix.size()-1;
-        int right = matrix[0].size()-1;
-        while(top<=bottom && left<=right){
-            int f = 0;
-            // cout<<"left : "<<left<<" "<<"Right : "<<right<<endl;
-            for(int i = left; i<=right ; i++){
-                f=1;
-                // cout<<top<<" "<<i<<endl;
+        int n = matrix.size(), m = matrix[0].size();
+        int top = 0, bottom = n - 1;
+        int left = 0, right = m - 1;
+        vector<int> ans;
+        while (top <= bottom and left <= right) {
+            for (int i = left; i <= right; i++) {
+                // cout << "right : " << matrix[top][i] << endl;
                 ans.push_back(matrix[top][i]);
-                // cout<<i<<" " <<matrix[top][i]<<"\n";
             }
             ++top;
-            
-            for(int i=top;i<=bottom;i++){
-                f=1;
+            for (int i = top; i <= bottom; i++) {
+                // cout << "bottom : " << matrix[i][right] << endl;
                 ans.push_back(matrix[i][right]);
-                // cout<<matrix[i][right]<<" ";
             }
             --right;
-            
-            if(top<=bottom){
-                for(int i=right; i>=left ;i--){
-                    f=1;
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    // cout << "left : " << matrix[bottom][i] << endl;
                     ans.push_back(matrix[bottom][i]);
-                    // cout<<matrix[bottom][i]<<" \n";
                 }
+
                 --bottom;
             }
-            if(left<=right){
-            for(int i= bottom; i>=top ; i--){
-                f=1;
-                ans.push_back(matrix[i][left]);
-                // cout<<matrix[i][left]<<" ";
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    // cout << "top : " << matrix[i][left] << endl;
+                    ans.push_back(matrix[i][left]);
+                }
+                ++left;
             }
-            ++left;
-            }
-            // if(!f) break;
         }
         return ans;
     }
