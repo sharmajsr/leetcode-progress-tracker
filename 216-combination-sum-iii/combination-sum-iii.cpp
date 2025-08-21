3f7fe54fc5f0775215cb4c7ba5a240eefdff1f67
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2025-08-22 02:38:16 +0530

    Time: 3 ms (15.89%) | Memory: 9.6 MB (20.95%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.cpp
+++ 216-combination-sum-iii/combination-sum-iii.cpp
@@ -1,34 +1,27 @@
 class Solution {
 public:
-    void printEl(vector<int>v){
-        for(auto i : v){
-            cout<<i<<" ";
-        }
-        cout<<endl;
-    }
-    void solve(vector<vector<int>>&ans , vector<int>path, int k, int n,int idx){
-        // cout<<n<<endl;
-        if( n == 0){
-            if(k == 0) {
-                ans.push_back(path);
+    void solve(vector<vector<int>>&res, vector<int>path, int n, int k , int idx){
+        if( n==0 ){
+            if( k==0){
+                res.push_back(path);
                 return;
             }
-            return ;
-        }
-        if(k == 0 ){
             return;
         }
-        for(int i= idx ; i<=9 ; i++){
-            if(n >= i){
+        if( k == 0 ){
+            return;
+        }
+        for(int i = idx ; i <= 9 ; i++){
+            if(n>=i ){
                 path.push_back(i);
-                solve(ans, path, k-1, n-i , i+1);
+                solve(res,path,n-i,k-1,i+1);
                 path.pop_back();
             }
         }
     }
     vector<vector<int>> combinationSum3(int k, int n) {
-        vector<vector<int>> ans;
-        solve(ans,{},k,n,1);
-        return ans;
+        vector<vector<int>>res;
+        solve(res,{},n,k,1);
+        return res;
     }
 };
