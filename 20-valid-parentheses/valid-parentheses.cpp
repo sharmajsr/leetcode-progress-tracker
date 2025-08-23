3af51cb5622fca5d76502e5f74161c7ee7ef1e76
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2025-08-24 05:00:07 +0530

    Time: 1 ms (15.58%) | Memory: 8.9 MB (33.27%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -2,32 +2,35 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<char>st;
-        for(auto i : s){
-            if(i == '(' or  i== '{' or i == '['){
+        for(auto i :  s) {
+            if(st.empty()){
                 st.push(i);
-            }
-            else if(!st.empty() and i == ')'){
-                if(st.top() == '('){
-                    st.pop();
-                }else{
-                    return false;
+            }else if(!st.empty()){
+                if(i == '(' or i== '{' or i=='['){
+                    st.push(i);
+                }else if(i == ')'){
+                    if(st.top() == '('){
+                        st.pop();
+                    }else{
+                        st.push(i);
+                    }
                 }
-            }else if(!st.empty() and i == '}'){
-                if(st.top() == '{'){
-                    st.pop();
-                }else{
-                    return false;
+                else if(i == '}'){
+                    if(st.top() == '{'){
+                        st.pop();
+                    }else{
+                        st.push(i);
+                    }
                 }
-            }else if(!st.empty() and i == ']'){
-                if(st.top() == '['){
-                    st.pop();
-                }else{
-                    return false;
+                else if(i == ']'){
+                    if(st.top() == '['){
+                        st.pop();
+                    }else{
+                        st.push(i);
+                    }
                 }
-            }else{
-                return false;
             }
         }
-        return st.size() == 0 ;
+        return st.empty();
     }
 };
