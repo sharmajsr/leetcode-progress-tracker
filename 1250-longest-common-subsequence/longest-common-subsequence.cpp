681afbf9f8bbf360abfcca94f9c580e5846f1aa8
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2025-08-25 01:52:10 +0530

    Time: 41 ms (27.78%) | Memory: 27.6 MB (15.20%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
-    vector<vector<int>>dp;
-    int lcs(string &s1, string &s2, int idx1, int idx2){
-        if(idx1 <0 or idx2 < 0){
-            return 0;
+    // ;
+    int lcs(string &s1, string &s2, int i, int j,vector<vector<int>>&dp){
+        // cout<<i<<" "<<j<<endl;
+
+        if(i == 0 or j == 0 ) return dp[i][j] = 0;
+
+        if(dp[i][j] != -1) return dp[i][j] ;
+
+        if(s1[i-1] == s2[j-1]){
+            return dp[i][j] = 1 + lcs(s1,s2, i-1,j-1,dp);
         }
-        if(dp[idx1][idx2] != -1)    return dp[idx1][idx2];
-        if(s1[idx1] == s2[idx2]){
-            return 1+ lcs(s1,s2,idx1-1,idx2-1);
-        }
-        return dp[idx1][idx2] = max(lcs(s1,s2,idx1-1,idx2), lcs(s1,s2,idx1,idx2-1 ));
+
+        return dp[i][j] = max(lcs(s1,s2,i-1,j,dp) , lcs(s1,s2,i,j-1,dp));
     }
     int longestCommonSubsequence(string s1, string s2) {
         int n = s1.size() , n2 = s2.size();
-        dp.resize(n,vector<int>(n2,-1));
-        return lcs(s1,s2,s1.size()-1,s2.size()-1);
+        vector<vector<int>>dp;
+        dp.resize(n+1,vector<int>(n2+1,-1));
+        return lcs(s1,s2,n,n2,dp);
     }
 };
