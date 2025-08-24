2563e0b8fbfc7dea82143b8b9f4e7c642d2aca69
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2025-08-24 23:13:05 +0530

    Time: 129 ms (59.70%) | Memory: 111.4 MB (14.47%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     bool solve(vector<int>&arr,int sum, int idx,vector<vector<int>>&dp){
-        if(sum == 0){
+        if(sum == 0 ){
             return true;
         }
-        if(idx == 0){
-            return arr[idx] == sum;
+        if(idx == 0 ){
+            return sum == arr[idx];
         }
-        if(dp[idx][sum] != -1)  return dp[idx][sum];
+        if(dp[idx][sum] != -1) return dp[idx][sum];
+
         bool not_take = solve(arr,sum,idx-1,dp);
         bool take = false;
         if(sum >= arr[idx]){
-            take = solve(arr,sum-arr[idx],idx-1,dp);
+            take = solve(arr, sum-arr[idx], idx-1,dp);
         }
-        
-        return dp[idx][sum]=  take or not_take;
+        return dp[idx][sum] = take or not_take;
     }
     bool canPartition(vector<int>& nums) {
         int tsum = 0;
