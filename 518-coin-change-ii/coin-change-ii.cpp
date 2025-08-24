341ff41ad58180bcf04f8046182bda2225be79cd
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2025-08-25 01:37:26 +0530

    Time: 15 ms (75.49%) | Memory: 28.3 MB (55.27%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,25 +1,31 @@
 class Solution {
 public:
+     int solve(vector<int>&coins, int amount, int idx, vector<vector<int>>&dp){
+        if(amount == 0 ){
+            return dp[idx][amount] = 1 ;
+        }
+        if(idx == 0 ){
+            if(amount%coins[idx] == 0 ){
+                return dp[idx][amount] = 1;
+            }
+            return 0;
+        }
+        if(dp[idx][amount] != -1) return dp[idx][amount];
+        int not_take = solve(coins , amount , idx-1, dp);
+        int take = 0, minTake = 1e9;
+        if(amount>= coins[idx]){
+            take = solve(coins,amount - coins[idx] , idx, dp);
+        }
+        return dp[idx][amount] = take + not_take;
+    }
+    
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
-        int t[n+1][amount+1];
-        for(int i=0;i<=n;i++){
-            for(int j=0;j<=amount;j++){
-                if( i == 0 )  
-                t[i][j] = 0; 
-                if( j == 0 )    t[i][j] = 1;
-            }
-        }
-        for(int i=1;i<=n;i++){
-            // t[i][0]=INT_MAX;
-            for(int j=1;j<=amount;j++){
-                if(j >= coins[i-1]){
-                    t[i][j] = t[i-1][j] + t[i][j-coins[i-1]];
-                }else{
-                    t[i][j] = t[i-1][j];
-                }
-            }
-        }
-        return t[n][amount] ;
+        vector<vector<int>>dp(n, vector<int>(amount+1,-1));
+        solve(coins,amount, n-1 , dp);
+        // if(dp[n-1][amount] == 1e9){
+        //     return -1;
+        // }
+        return dp[n-1][amount] == -1 ? 0 :  dp[n-1][amount] ;
     }
 };
