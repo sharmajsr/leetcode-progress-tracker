504693b2b0ff0c41f0d4d5ebbb096d366a32adb6
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2025-08-25 02:53:38 +0530

    Time: 0 ms (100.00%) | Memory: 27.5 MB (82.26%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.cpp
+++ 695-max-area-of-island/max-area-of-island.cpp
@@ -1,30 +1,32 @@
 class Solution {
 public:
-    void solve(int x,int y,vector<vector<int>>&grid,vector<vector<int>>&vis,int &temp){
-        int rows[] = { 0,0 ,1, -1} , cols[] = {1,-1,0,0} ;
-        vis[x][y]=1;
-        ++temp;
-        for(int i =0;i < 4;i++){
-            int nr = rows[i] + x;
-            int nc = cols[i] + y;
-            if(nr >=0 and nc>=0 and nr < grid.size() and nc < grid[0].size() and grid[nr][nc] == 1 and vis[nr][nc] == 0){
-                solve(nr,nc,grid,vis,temp);
+    void solve(vector<vector<int>>& grid, int r , int c ,int &cur){
+        int rows[] = { 0,0,1,-1};
+        int cols[] = {1,-1,0,0};
+        for(int i=0;i<4; i++){
+            int nr = r+ rows[i];
+            int nc = c + cols[i];
+            if(nr >=0 and nr  < grid.size() and nc >=0 and nc < grid[0].size() and grid[nr][nc] == 1){
+                grid[nr][nc] = 0 ;
+                cur +=1 ;
+                solve(grid,nr,nc,cur);
             }
         }
     }
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-        int maxArea = 0;
         int n = grid.size() , m = grid[0].size();
-        vector<vector<int>>vis(n,vector<int>(m,0));
-        for(int i = 0 ; i < n ;i++){
-            for(int j= 0 ; j <m ; j++){
-                if(grid[i][j] == 1 and vis[i][j] == 0 ){
-                    int temp = 0 ;
-                    solve(i,j,grid,vis,temp);
-                    maxArea = max(maxArea,temp);
+        int ans = 0;
+        for(int i = 0; i < n ; i++){
+            for(int j=0; j<m ;j++){
+                if(grid[i][j] == 1){
+                    int cur = 1 ;
+                    grid[i][j]=0;
+                    solve(grid, i , j, cur );
+                    // cout<<i<<" "<<j<<" "<<cur<<endl;
+                    ans = max(ans,cur);
                 }
             }
         }
-        return maxArea;
+        return ans;
     }
 };
