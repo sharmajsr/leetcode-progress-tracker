2c2f87c791b205035e1d6d0a23e1cebaeac267d1
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2025-08-25 08:00:41 +0530

    Time: 43 ms (22.56%) | Memory: 52.8 MB (5.49%) - LeetSync

--- 621-task-scheduler/task-scheduler.cpp
+++ 621-task-scheduler/task-scheduler.cpp
@@ -1,26 +1,44 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        int time = 0 ; 
-        int max_element =0 ; 
-        int max_count = 0; 
-        priority_queue<pair<int,int>>pq;
-        unordered_map<char,int>um ; 
+        if ( n == 0 )   return tasks.size();
+        int k = n , ans= 0 ;
+        unordered_map<int,int>um;
+
+        priority_queue<pair<int,int>>maxH;
+
         for(auto i : tasks){
-            ++um[i];
+            ++um[i-'a'];
         }
-        for(auto i: um ){
-            if( i.second > max_element ) {
-                 max_element = i.second;
+        for(auto i : um){
+            maxH.push({i.second,i.first});
+        }
+        while(!maxH.empty()){
+            vector<pair<int,int>>temp;
+            for(int i = 0 ; i <= n ;i++){
+                if(!maxH.empty()){
+                    auto top = maxH.top();
+                    maxH.pop();
+                    temp.push_back(top);
+                }
+            }
+            for(auto i : temp){
+                if(i.first-1 > 0)
+                maxH.push({i.first-1,i.second});
+            }
+
+            if(!maxH.empty()){
+                ans += (n+1);
+            }else{
+                ans += temp.size();
             }
         }
-        for(auto i : um ){
-            if(i.second == max_element) ++max_count;
-        }
-        int nval = (max_element - 1 ) * (n+1) + max_count;
-        int ans = tasks.size();
-        if( nval > ans) ans = nval;
         return ans;
-        
     }
 };
+
+// A - 3 , B - 3 
+// ["A","C","A","B","D","B"]
+// A -2  , C - 1, B - 2, D -1
+// A -2, B -2 , C - 1 , D - 1
+
