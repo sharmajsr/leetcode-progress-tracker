38368546eb0d3e21b014afe72c4b64bd91758e5d
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2025-09-10 19:00:45 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (72.49%) - LeetSync

--- 92-reverse-linked-list-ii/reverse-linked-list-ii.cpp
+++ 92-reverse-linked-list-ii/reverse-linked-list-ii.cpp
@@ -10,66 +10,69 @@
  */
 class Solution {
 public:
-    ListNode* reverseList(ListNode* head){
+    ListNode* reverse(ListNode* head){
         ListNode* prev = NULL;
         ListNode* next;
-        while(head!=NULL){
+        while(head){
             next = head->next;
-            head->next= prev;
+            head->next = prev;
             prev = head;
-            head=next;
+            head= next;
         }
         return prev;
     }
-    void printList(ListNode* head){
+    ListNode* findRightNode(ListNode* head, int right){
         while(head){
-            cout<<head->val<<" ";
-            head= head->next;
-        }cout<<endl;
+            --right;
+            cout<<right<<" "<<head->val<<endl;
+            if(right == 1 ){
+                return head;
+            }
+            head = head->next;
+        }
+        return head;
     }
     ListNode* reverseBetween(ListNode* head, int left, int right) {
-        if(left == right)   return head;
-        
+        if(!head or left == right) {
+            return head;
+        }
+        int k = 1;
         ListNode* dummyNode = new ListNode();
         dummyNode->next = head;
-        ListNode* lprev;
-        ListNode* rprev;
-        bool lfind = false, rfind = false;
-        if(left == 1){
-            lfind= true;
-            lprev = head;
+        if( left == 1 ){
+            cout<<head->val<<endl;
+            ListNode* r1 = findRightNode(head->next, right);
+            cout<<r1->val<<endl;
+            ListNode* next = r1->next;
+            r1->next = NULL;
+            ListNode* rev = reverse(head);
+            ListNode* rev1 = rev;
+            while(rev->next !=NULL){
+                rev = rev->next;
+            }
+            rev->next = next;
+            dummyNode->next = rev1;
+        }else{
+            while(head){
+                if(left == k+1){
+                    // cout<<"Head next val "<<head->next->val<<endl;
+                    ListNode* r1 = findRightNode(head->next->next, right-left+1);
+                    // cout<<"Rece1 : "<<r1->val<<endl;
+                    ListNode* next = r1->next;
+                    r1->next = NULL;
+                    ListNode* rev = reverse(head->next);
+                    ListNode* rev1 = rev;
+                    while(rev->next !=NULL){
+                        rev = rev->next;
+                    }
+                    rev->next = next;
+                    head->next = rev1;
+                    break;
+                }
+                k += 1;
+                head=head->next;
+            }
         }
-        int cnt = 0; 
-        while(head!=NULL){
-            ++cnt;
-
-            if(cnt == left)   lfind = true;
-            if(cnt == right)  rfind = true;
-
-            if(!lfind)  lprev = head;
-            if(!rfind)  rprev = head;
-
-            head=head->next;
-        }
-        ListNode* robj = rprev->next->next;
-        rprev->next->next = NULL;
-        cout<<"lprev next "<<lprev->next->val<<endl;
-        ListNode* rev ;
-        if(left != 1){
-            rev  = reverseList(lprev->next);
-            lprev->next = rev;
-        } 
-        else{
-            rev  = reverseList(lprev);
-            lprev = rev;
-            dummyNode->next = lprev;
-        }    
-        printList(robj);
-        
-        while(lprev->next !=NULL){
-            lprev = lprev->next;
-        }
-        lprev->next = robj;
         return dummyNode->next;
     }
 };
