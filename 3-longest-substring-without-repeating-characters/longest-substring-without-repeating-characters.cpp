aa2cae10a6f9ec2971ebb07407256466fe772801
sharmajsr <31768622+sharmajsr@users.noreply.github.com>
2025-09-11 01:19:56 +0530

    Time: 641 ms (5.01%) | Memory: 247.4 MB (5.00%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,30 +1,39 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        unordered_map<char,int>um;
+        
         int i = 0, j=0 , n= s.size() , maxLen = 0;
-        while(j<n){
-            ++um[s[j]];
-            if(j-i+1 == um.size()){
-                if(maxLen < j-i+1){
-                    maxLen = j-i+1;
+        for(int i=0 ; i<n ; i++){
+            unordered_map<char,int>um;
+            for(int j=i ; j<n ; j++){
+                ++um[s[j]];
+                if(um[s[j]] > 1){
+                    break;
                 }
-                ++j;
-            }else if(um.size() < j-i+1){
-                while(j-i+1 != um.size()){
-                    --um[s[i]];
-                    if(um[s[i]] == 0 ){
-                        um.erase(s[i]);
-                    }
-                    ++i;
-                }
-                if(maxLen == j-i+1){
-                    maxLen = j-i+1;
-                }
-                ++j;
-
+                maxLen = max(maxLen , j-i+1);
             }
         }
+        // while(j<n){
+        //     ++um[s[j]];
+        //     if(j-i+1 == um.size()){
+        //         if(maxLen < j-i+1){
+        //             maxLen = j-i+1;
+        //         }
+        //         ++j;
+        //     }else if(um.size() < j-i+1){
+        //         while(j-i+1 != um.size()){
+        //             --um[s[i]];
+        //             if(um[s[i]] == 0 ){
+        //                 um.erase(s[i]);
+        //             }
+        //             ++i;
+        //         }
+        //         if(maxLen == j-i+1){
+        //             maxLen = j-i+1;
+        //         }
+        //         ++j;
+        //     }
+        // }
         return maxLen;
     }
 };
